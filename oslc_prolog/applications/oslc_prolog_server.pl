6dc36f1516955095cf4a0a3d44f071d433b3047b
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2017-06-29 16:50:15 +0200

    handling of content-types, served prefix setting, simple response population (flat list)

--- oslc_prolog/applications/oslc_prolog_server.pl
+++ oslc_prolog/applications/oslc_prolog_server.pl
@@ -2,25 +2,23 @@
 
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_files)).
-:- use_module(library(http/http_parameters)).
-:- use_module(library(http/json)).
-:- use_module(library(http/http_json)).
-:- use_module(library(http/http_error)).
 :- use_module(library(broadcast)).
+:- use_module(library(semweb/turtle)).
+:- use_module(library(semweb/rdf_persistency)).
 
 :- setting(oslc_prolog_server:base_uri, atom, 'http://localhost:3020/oslc/', 'Base URI').
+:- setting(oslc_prolog_server:served_prefix, atom, '', 'Served Prefix').
 
 :- http_handler('/oslc/', dispatcher, [prefix]).
 
 :- listen(settings(changed(oslc_prolog_server:base_uri, Old, New)), (
      uri_components(New, uri_components(Schema, Authority, NewPath, _, _)),
-     (  sub_atom(NewPath, _, 1, 0, '/')
-     -> http_handler(NewPath, dispatcher, [prefix]),
+     (  sub_atom(NewPath, _, 1, 0, '/') % if new base URI is ending with '/'
+     -> http_handler(NewPath, dispatcher, [prefix]), % set a handler for new base URI
         uri_components(Old, uri_components(_, _, OldPath, _, _)),
-        http_delete_handler(path(OldPath))
-     ;  atomic_list_concat([Schema, '://', Authority, NewPath, '/'], '', NewSetting),
-        set_setting(oslc_prolog_server:base_uri, NewSetting)
+        http_delete_handler(path(OldPath)) % remove old handler
+     ;  atomic_list_concat([Schema, '://', Authority, NewPath, '/'], '', NewSetting), % append '/' to the new URI
+        set_setting(oslc_prolog_server:base_uri, NewSetting) % do set setting again, it will call this listener
      )
    )).
 
@@ -30,27 +28,63 @@ dispatcher(Request) :-
       member(host(Host), Request),
       member(port(Port), Request),
       member(path(Path), Request),
-      atomic_list_concat([Protocol, '://', Host, ':', Port,  Path], '', Uri),
+      atomic_list_concat([Protocol, '://', Host, ':', Port,  Path], '', Uri), % determine URI called
       setting(oslc_prolog_server:base_uri, Prefix),
-      atom_concat(Prefix, ServicePath, Uri),
-      split_string(ServicePath, '/', '/', Parts),
-      dispatch(Request, Parts)
+      atom_concat(Prefix, ServicePath, Uri), % check if URI called start with base URI
+      dispatch(Request, ServicePath) % call dispatch with computed suffix
     )
   ->  true
   ; format('Status: 404~n~n')
   ).
 
-dispatch(_, Parts) :-
-  format('Status: 200~n'),
-  format('Content-type: application/rdf+xml; charset=utf-8~n~n'),
+dispatch(Request, ServicePath) :-
   current_output(Out),
-  setting(oslc_prolog_server:base_uri, Prefix),
-  atomic_list_concat(Parts, '/', Suffix),
-  atom_concat(Prefix, Suffix, Resource),
-  rdf_save_header(Out, [namespaces([rdf])]),
-  rdf_save_subject(Out, Resource, [nsmap([])]),
-  rdf_save_footer(Out).
+  setting(oslc_prolog_server:served_prefix, Prefix), % fetch served RDF prefix from settings
+  atom_concat(Prefix, ServicePath, Resource), % compute served RDF resource URI
+  setup_call_cleanup(make_graph(Resource, Graph), ( % create and populate new temporary RDF graph
+    rdf_graph_property(Graph, triples(Triples)),
+    Triples > 0,
+    select_content_type(Request, ContentType), % compute content-type to serve based on request
+    ( ContentType == nil
+    -> format('Status: 415~n~n') % requested content-type cannot be served
+    ;  format('Status: 200~n'),
+       format('Content-type: ~w; charset=utf-8~n~n', [ContentType]),
+       save_graph(Out, Graph, ContentType) % serialize temporary RDF graph to the response
+    )
+  ), rdf_unload_graph(Graph)). % regardless of the result remove temporary RDF graph
 
-% rdf_save(stream(current_output), [graph(mygraph)]).
-% rdf_save_turtle(stream(current_output), [graph(mygraph)]).
-% current_output(Out), rdf_save_subject(Out, 'http://my.com/catalog', [nsmap([])]).
+make_graph(Resource, Graph) :-
+  uuid(Graph), % generate unique identifier for the temporary RDF graph
+  rdf_create_graph(Graph),
+  rdf_persistency(Graph, false), % do not persist (keep only in RAM)
+  forall(rdf(Resource, P, O), rdf_assert(Resource, P, O, Graph)). % poor man's populate method
+
+uuid(Id) :-
+  Max is 1<<128,
+  random_between(0, Max, Num),
+  atom_number(Id, Num).
+
+select_content_type(Request, ContentType) :-
+  member(accept(Accept), Request),
+  predsort(accept_compare, Accept, AcceptSorted), % sort requested content types according to qualities
+  select_content_type0(AcceptSorted, ContentType).
+
+select_content_type0([], nil) :- !.
+select_content_type0([media(H,_,_,_)|T], ContentType) :- % go through all requested content types
+  ( member(H, [application/'rdf+xml', text/'turtle', application/'x-turtle'])
+  -> ContentType = H % if content type is */* then H becomes _VAR/_VAR and matches application/'rdf+xml'
+  ; select_content_type0(T, ContentType)
+  ).
+
+accept_compare(<, media(_,_,W1,_), media(_,_,W2,_)) :- % sort qualities in reverse order - biggest first
+  W1 > W2.
+accept_compare(<, _, _).
+
+save_graph(Out, Graph, application/'rdf+xml') :-
+  rdf_save(stream(Out), [graph(Graph)]).
+
+save_graph(Out, Graph, text/'turtle') :-
+  rdf_save_turtle(Out, [graph(Graph)]).
+
+save_graph(Out, Graph, application/'x-turtle') :-
+  rdf_save_turtle(Out, [graph(Graph)]).
