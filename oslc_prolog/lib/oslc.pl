1b671515a007183998d7f9de20739c7ddea70711
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2017-07-10 17:04:07 +0200

    second refactoring to remove duplicate shape definition from memory

--- oslc_prolog/lib/oslc.pl
+++ oslc_prolog/lib/oslc.pl
@@ -1,82 +1,73 @@
 :- module(oslc, [
-  oslc_resource/5,
-  oslc_property/5
+  register_resource/2,
+  oslc_resource/4,
+  oslc_properties/4
 ]).
 
 :- use_module(library(semweb/rdf_library)).
 
 :- rdf_register_prefix(oslc, 'http://open-services.net/ns/core#').
-:- rdf_register_prefix(eos, 'http://ontology.cf.ericsson.net/eos#').
+:- rdf_register_prefix(oslcs, 'http://ontology.cf.ericsson.net/oslc_shapes#').
+:- rdf_register_prefix(oslcp, 'http://ontology.cf.ericsson.net/oslc_prolog#').
 
 :- rdf_attach_library(oslc_prolog(rdf)).
 :- rdf_load_library(oslc).
-:- rdf_load_library(eos).
+:- rdf_load_library(oslcs).
+:- rdf_load_library(oslcp).
+
+:- rdf_meta register_resource(r, -).
+:- rdf_meta oslc_resource(r, r, t, -).
+:- rdf_meta oslc_properties(r, r, t, -).
 
 :- initialization init.
 
-graph('http://ontology.cf.ericsson.net/eos').
-
 init :-
-  graph(Graph),
-  forall(rdf(X, rdf:type, eos:'PrologPredicate', Graph), ignore(register_predicate(oslc, X))).
+  forall(
+    rdf(X, rdf:type, oslcp:'PrologPredicate', 'http://ontology.cf.ericsson.net/oslc_prolog'),
+    register_resource(X, oslc)
+  ).
 
-register_predicate(Module, Predicate) :-
-  graph(Graph),
-  rdf(Predicate, eos:prologName, literal(type(rdf:'XMLLiteral', Name)), Graph),
-
-  P =.. [Name, Spec, POptions, PGraph],
+register_resource(PredicateResource, Module) :-
+  rdf(PredicateResource, oslcp:prologName, literal(type(xsd:string, PredicateName))),
+  rdf(PredicateResource, oslc:resourceShape, ResourceShape),
+  P =.. [PredicateName, Spec, Options, Graph],
   retractall(Module:P),
-
-  rdf(Predicate, oslc:resourceShape, Shape, Graph),
-  rdf(Shape, oslc:describes, PResource, Graph),
-
-  findall(X, (
-    rdf(Shape, oslc:property, Property, Graph),
-    rdf(Property, eos:prologName, literal(type(rdf:'XMLLiteral', PropertyName)), Graph),
-    findall(Y, (
-      rdf(Property, PropertyProperty, PropertyValue, Graph),
-      Y =.. [PropertyProperty, PropertyValue]
-    ), PPs),
-    dict_create(Z, _, PPs),
-    X =.. [PropertyName, Z]
-  ), Ps),
-
-  dict_create(PDict, _, Ps),
-
   assertz((
     Module:P :-
-      rdf_global_id(Spec, PIRI),
-      oslc_resource(PIRI, PResource, PDict, POptions, PGraph)
-  )).
-
-oslc_resource(PIRI, PResource, PDict, POptions, PGraph) :-
-  rdf_transaction((
-    rdf_assert(PIRI, rdf:type, PResource, PGraph),
-    oslc_property(PIRI, PResource, PDict, POptions, PGraph)
-  )).
-
-oslc_property(_, _, _, [], _) :- !.
-oslc_property(PIRI, PResource, PDict, [H|T], PGraph) :-
-  H =.. [Property, Value],
-  ignore((
-    PP = PDict.get(Property),
-    rdf_global_id(oslc:propertyDefinition, OPD),
-    ( var(Value)
-    -> rdf(PIRI, PP.get(OPD), FormattedValue, PGraph),
-      format_value(Value, _, FormattedValue)
-    ; rdf_global_id(oslc:valueType, OVT),
-      format_value(Value, PP.get(OVT), FormattedValue),
-      rdf_assert(PIRI, PP.get(OPD), FormattedValue, PGraph)
-    )
+      rdf_global_id(Spec, IRI),
+      oslc_resource(IRI, ResourceShape, Options, Graph)
   )),
-  oslc_property(PIRI, PResource, PDict, T, PGraph).
+  Module:export(PredicateName/3).
 
-format_value(Value, Type, literal(type(Type, Value))) :-
-  rdf_global_id(rdf:'XMLLiteral', T1),
-  rdf_global_id(xsd:string, T2),
-  member(Type, [T1, T2]), !.
+oslc_resource(IRI, ResourceShape, Options, Graph) :-
+  rdf_transaction((
+    rdf(ResourceShape, oslc:describes, Resource),
+    rdf_assert(IRI, rdf:type, Resource, Graph),
+    oslc_properties(IRI, ResourceShape, Options, Graph)
+  )).
 
-format_value(Value, _, Value).
+oslc_properties(_, _, [], _).
+oslc_properties(IRI, ResourceShape, [H|T], Graph) :-
+  once((
+    H =.. [Property, Value]
+  ; H = (Property=Value)
+  )),
+  rdf(ResourceShape, oslc:property, PropertyResource),
+  rdf(PropertyResource, oslcp:prologName, literal(type(xsd:string, Property))),
+  rdf(PropertyResource, oslc:propertyDefinition, PropertyDefinition),
+  rdf(PropertyResource, oslc:valueType, Type),
+  ( var(Value)
+  -> rdf(IRI, PropertyDefinition, ReadValue, Graph),
+     format_value(ReadValue, Type, Value)
+  ;  format_value(WriteValue, Type, Value),
+     rdf_assert(IRI, PropertyDefinition, WriteValue, Graph)
+  ),
+  oslc_properties(IRI, ResourceShape, T, Graph).
+
+format_value(Value, 'http://www.w3.org/2000/01/rdf-schema#Resource', Value).
+
+format_value(literal(type(Type, Value)), Type, Value).
+
 
 %iri_xml_namespace(PropertyProperty, NS, Local),
 %rdf_current_prefix(Prefix, NS),
