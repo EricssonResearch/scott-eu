907bf01c4a327e9c8d0890c6c9a6174fc8b71af3
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2017-07-21 14:37:44 +0200

    first dispatcher works

--- oslc_prolog/lib/oslc_dispatch.pl
+++ oslc_prolog/lib/oslc_dispatch.pl
@@ -7,8 +7,12 @@
   oslc_put/3,
   oslc_delete/2,
   oslc_delete/3,
+  add_handler/4,
+  delete_handler/2,
   dispatch/4
 ]).
+
+:- use_module(library(oslc_error)).
 
 :- meta_predicate(oslc_get(+, 2)).
 :- meta_predicate(oslc_get(+, 2, +)).
@@ -19,35 +23,90 @@
 :- meta_predicate(oslc_delete(+, 1)).
 :- meta_predicate(oslc_delete(+, 1, +)).
 
-oslc_get(IRI, Predicate) :-
-  oslc_get(IRI, Predicate, 1000).
+:- dynamic handler/4.
 
-oslc_get(IRI, Predicate, Priority) :-
-  add_handler(get, IRI, Predicate, Priority).
+oslc_get(IRISpec, Predicate) :-
+  oslc_get(IRISpec, Predicate, 1000).
 
-oslc_post(IRI, Predicate) :-
-  oslc_post(IRI, Predicate, 1000).
+oslc_get(IRISpec, Predicate, Priority) :-
+  add_handler(get, IRISpec, Predicate, Priority).
 
-oslc_post(IRI, Predicate, Priority) :-
-  add_handler(post, IRI, Predicate, Priority).
+oslc_post(IRISpec, Predicate) :-
+  oslc_post(IRISpec, Predicate, 1000).
 
-oslc_put(IRI, Predicate) :-
-  oslc_put(IRI, Predicate, 1000).
+oslc_post(IRISpec, Predicate, Priority) :-
+  add_handler(post, IRISpec, Predicate, Priority).
 
-oslc_put(IRI, Predicate, Priority) :-
-  add_handler(put, IRI, Predicate, Priority).
+oslc_put(IRISpec, Predicate) :-
+  oslc_put(IRISpec, Predicate, 1000).
 
-oslc_delete(IRI, Predicate) :-
-  oslc_delete(IRI, Predicate, 1000).
+oslc_put(IRISpec, Predicate, Priority) :-
+  add_handler(put, IRISpec, Predicate, Priority).
 
-oslc_delete(IRI, Predicate, Priority) :-
-  add_handler(delete, IRI, Predicate, Priority).
+oslc_delete(IRISpec, Predicate) :-
+  oslc_delete(IRISpec, Predicate, 1000).
 
-add_handler(Method, IRI, Predicate, Priority) :-
-  format(current_output, '~w: ~w -> ~w [~w]~n', [Method, IRI, Predicate, Priority]).
+oslc_delete(IRISpec, Predicate, Priority) :-
+  add_handler(delete, IRISpec, Predicate, Priority).
 
+add_handler(Method, IRISpec, Predicate, Priority) :-
+  must_be(atom, Method),
+  must_be(ground, IRISpec),
+  must_be(ground, Predicate),
+  must_be(integer, Priority),
+  ( handler(Method, IRISpec, _, Priority)
+  -> upcase_atom(Method, UMethod),
+     oslc_error('Cannot redefine [~w] handler for [~w] with priority [~w]', [UMethod, IRISpec, Priority])
+  ; assertz(handler(Method, IRISpec, Predicate, Priority))
+  ).
 
-dispatch(Prefix:Resource, Request, GraphIn, GraphOut) :-
-  format('Status: 200~n'),
-  format('Content-type: text/plain; charset=utf-8~n~n'),
-  format('~w:~w ~w ~w ~w~n', [Prefix, Resource, Request, GraphIn, GraphOut]).
+delete_handler(Method, IRISpec) :-
+  forall(
+    handler(Method, IRISpec, Predicate, Priority),
+    retractall(handler(Method, IRISpec, Predicate, Priority))
+  ).
+
+dispatch(Prefix:ResourceSegments, Request, GraphIn, GraphOut) :-
+  member(method(Method), Request),
+  atomic_list_concat(ResourceSegments, '/', Resource),
+  findall(
+    handler(Method, IRI, Predicate, Priority), (
+      handler(Method, ISPrefix:ISResource, Predicate, Priority),
+      match_wildcard(ISPrefix, Prefix, ISResource, Resource),
+      rdf_global_id(Prefix:Resource, IRI)
+    ),
+    Handlers
+  ),
+  predsort(handler_compare, Handlers, [handler(Method, IRI, Predicate, _)|_]),
+  dispatch_to_handler(Method, Predicate, IRI, GraphIn, GraphOut).
+
+match_wildcard(ISPrefix, Prefix, ISResource, Resource) :-
+  once((
+    ISPrefix == Prefix
+  ; ISPrefix == *
+  )),
+  once((
+    ISResource == Resource
+  ; ISResource == *
+  )).
+
+handler_compare(<, handler(_,_,_,P1), handler(_,_,_,P2)) :-
+  P1 >= P2.
+handler_compare(>, handler(_,_,_,P1), handler(_,_,_,P2)) :-
+  P1 < P2.
+
+dispatch_to_handler(get, Module:Predicate, IRI, _, GraphOut) :-
+  T =.. [Predicate, IRI, GraphOut],
+  call(Module:T).
+
+dispatch_to_handler(post, Module:Predicate, IRI, GraphIn, GraphOut) :-
+  T =.. [Predicate, IRI, GraphIn, GraphOut],
+  call(Module:T).
+
+dispatch_to_handler(put, Module:Predicate, IRI, GraphIn, GraphOut) :-
+  T =.. [Predicate, IRI, GraphIn, GraphOut],
+  call(Module:T).
+
+dispatch_to_handler(delete, Module:Predicate, IRI, _, _) :-
+  T =.. [Predicate, IRI],
+  call(Module:T).
--- oslc_prolog/lib/oslc_example.pl
+++ oslc_prolog/lib/oslc_example.pl
@@ -9,6 +9,15 @@
 :- rdf_load_library(oslc_example).
 
 :- oslc_get(oslc_example:leoResourceShape, handle_get).
+:- oslc_post(oslc_example:(*), handle_post_put).
+:- oslc_put(oslc_example:leoResourceShape, handle_post_put).
+:- oslc_delete(oslc_example:leoResourceShape, handle_delete).
 
 handle_get(_IRI, _GraphOut) :-
   format('Status: 416~n~n').
+
+handle_post_put(_IRI, _GraphIn, _GraphOut) :-
+  format('Status: 417~n~n').
+
+handle_delete(_IRI) :-
+  format('Status: 418~n~n').
