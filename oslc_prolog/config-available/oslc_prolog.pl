e47ec0b62da77470949a7e0913e5a4e69e52afa4
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2017-08-04 16:32:59 +0200

    reworked ontology declaration and loading procedures

--- oslc_prolog/config-available/oslc_prolog.pl
+++ oslc_prolog/config-available/oslc_prolog.pl
@@ -3,11 +3,17 @@
 /** <module> OSLC Prolog
 */
 
+%:- debug(http(send_request)).
+%:- debug(http(open)).
+%:- debug(rdf(load)).
+
 :- use_module(library(semweb/rdf_library)).
 
 :- rdf_attach_library(oslc_prolog(rdf)).
 :- rdf_load_library(oslc).
-:- rdf_load_library(oslc_shapes).
+
+:- use_module(library(oslc_ontology)).
+:- register_ontology(oslc_shapes).
 
 :- use_module(library(oslc)).
 :- use_module(library(oslc_core)).
--- oslc_prolog/lib/oslc_core.pl
+++ oslc_prolog/lib/oslc_core.pl
@@ -87,8 +87,12 @@ post_resource(IRI, Source, Sink) :-
     )),
     copy_resource(NewResource, NewResource, Source, Sink, []),
     setting(oslc_prolog_server:base_uri, BaseUri),
+    uri_components(BaseUri, uri_components(C1, C2, PrefixPath, C4, C5)),
+    split_string(PrefixPath, "/", "/", Parts),
     rdf_global_id(Prefix:Name, NewResource),
-    atomic_list_concat([BaseUri, Prefix, '/', Name],  Location),
+    append(Parts, [Prefix, Name], NewParts),
+    atomics_to_string(NewParts, '/', NewPath),
+    uri_components(Location, uri_components(C1, C2, NewPath, C4, C5)),
     response(201, ['Location'(Location)]) % created
   ),
     oslc_error(Message),
