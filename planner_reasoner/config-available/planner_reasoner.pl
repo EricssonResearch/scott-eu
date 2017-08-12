22624062ab8e3c0a0a9867486f4b02ff161be853
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2017-08-12 17:09:12 +0200

    joined ttls, removed old files, added license header

--- planner_reasoner/config-available/planner_reasoner.pl
+++ planner_reasoner/config-available/planner_reasoner.pl
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 Ericsson AB
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 :- module(conf_planner_reasoner, []).
 
 /** <module> Planner Reasoner
@@ -6,13 +22,9 @@
 :- use_module(library(semweb/rdf_library)).
 :- rdf_attach_library(planner_reasoner(rdf)).
 
-:- rdf_register_prefix(sh, 'http://www.w3.org/ns/shacl#').
-
 :- rdf_load_library(pddl).
-:- rdf_load_library(pddl_shapes).
-:- rdf_load_library(pddl_domain_example).
-:- rdf_load_library(pddl_problem_example).
 :- rdf_load_library(planner).
+:- rdf_load_library(pddl_example).
 
 :- use_module(library(pddl_generator)).
 
