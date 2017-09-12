3e391c1f58bab1ed91f34026161ce1e2df9a88b1
amadeu <amadeu.nascimento.junior@ericsson.com>
2017-09-12 11:56:34 -0300

    Bug-fix: linear and angular velocities; parameter tunning; new actuation scheme

--- turttlebot2_v4_turtlebot.lua
+++ turttlebot2_v4_turtlebot.lua
@@ -4,34 +4,6 @@ function setVels_cb(msg)
    local rotVel = msg.angular.z*interWheelDistance/2 -- in rad/s
    velocityRight = linVel+rotVel
    velocityLeft = linVel-rotVel
-   if simulationIsKinematic then
-        -- Simulation is kinematic
-        p=sim.boolOr32(sim.getModelProperty(objHandle),sim.modelproperty_not_dynamic)
-        sim.setModelProperty(objHandle,p)
-        dt=sim.getSimulationTimeStep()
-        p=sim.getJointPosition(leftJoint)
-        sim.setJointPosition(leftJoint,p+velocityLeft*dt*2/wheelDiameter)
-        p=sim.getJointPosition(rightJoint)
-        sim.setJointPosition(rightJoint,p+velocityRight*dt*2/wheelDiameter)
-        linMov=dt*(velocityLeft+velocityRight)/2.0
-        rotMov=dt*math.atan((velocityRight-velocityLeft)/interWheelDistance)
-        position=sim.getObjectPosition(objHandle,sim.handle_parent)
-        orientation=sim.getObjectOrientation(objHandle,sim.handle_parent)
-        xDir={math.cos(orientation[3]),math.sin(orientation[3]),0.0}
-        position[1]=position[1]+xDir[1]*linMov
-        position[2]=position[2]+xDir[2]*linMov
-        orientation[3]=orientation[3]+rotMov
-        sim.setObjectPosition(objHandle,sim.handle_parent,position)
-        sim.setObjectOrientation(objHandle,sim.handle_parent,orientation)
-    else
-        -- Simulation is dynamic
-        p=sim.boolOr32(sim.getModelProperty(objHandle),sim.modelproperty_not_dynamic)-sim.modelproperty_not_dynamic
-        sim.setModelProperty(objHandle,p)
-        velocityRight = linVel + rotVel
-        velocityLeft = linVel - rotVel
-        sim.setJointTargetVelocity(leftJoint,velocityLeft*2/wheelDiameter*180/math.pi)
-        sim.setJointTargetVelocity(rightJoint,velocityRight*2/wheelDiameter*180/math.pi)
-    end
 end
 
 if (sim_call_type==sim.childscriptcall_initialization) then 
@@ -42,18 +14,23 @@ if (sim_call_type==sim.childscriptcall_initialization) then
     
     leftJoint=sim.getObjectHandle("turtlebot_leftWheelJoint_")
     rightJoint=sim.getObjectHandle("turtlebot_rightWheelJoint_")
-    
     simulationIsKinematic=false -- we want a dynamic simulation here!
+    velocityLeft = 0
+    velocityRight = 0
+    linVel = 0
+    rotVel = 0
+
     -- Braitenberg weights:
     brait_left={0,-0.5,-1.25,-1,-0.2}
 
     t_frontBumper = sim.getObjectHandle('bumper_front_joint')
     t_rightBumper = sim.getObjectHandle('bumper_right_joint')
     t_leftBumper  = sim.getObjectHandle('bumper_left_joint')
-
+    r_linear_velocity, r_angular_velocity = {0,0,0},{0,0,0}
     originMatrix = sim.getObjectMatrix(mainBodyHandle,-1)
     invOriginMatrix = simGetInvertedMatrix(originMatrix)
-
+    oldTransformation = originMatrix
+    lastTime = simGetSimulationTime()
     ----------------------------- ROS STUFF --------------------------------
     -- Odometry
     pubPose = simROS.advertise(modelBaseName..'/pose','nav_msgs/Odometry')
@@ -100,13 +77,31 @@ if (sim_call_type == sim.childscriptcall_sensing) then
             end
 
         -- Odometry
-        local transformNow = sim.getObjectMatrix(mainBodyHandle,-1)
-        local pose_orientationNow = sim.multiplyMatrices(invOriginMatrix, transformNow)
-        local r_quaternion = simGetQuaternionFromMatrix(pose_orientationNow)
-        local r_position = {pose_orientationNow[4], pose_orientationNow[8], pose_orientationNow[12]}
-        local r_linear_velocity, r_angular_velocity = 0,0
-        r_linear_velocity, r_angular_velocity = simGetObjectVelocity(mainBodyHandle)
+        timeNow = simGetSimulationTime()
+        transformNow = sim.getObjectMatrix(mainBodyHandle,-1)
+        pose_orientationNow = sim.multiplyMatrices(invOriginMatrix, transformNow)
+        r_quaternion = simGetQuaternionFromMatrix(pose_orientationNow)
+        r_position = {pose_orientationNow[4], pose_orientationNow[8], pose_orientationNow[12]}
 
+        dt = (timeNow - lastTime)
+	if dt>0.1 then
+            r_linear_velocity, r_angular_velocity = {0,0,0},{0,0,0}
+        --r_linear_velocity, r_angular_velocity = simGetObjectVelocity(mainBodyHandle)
+	    oldInv = simGetInvertedMatrix(oldTransformation)
+            m = sim.multiplyMatrices(oldInv,transformNow)
+            aux_orientation = sim.getEulerAnglesFromMatrix(m)
+            aux_position = {m[4],m[8],m[12]}
+            r_angular_velocity[1] = aux_orientation[1]/dt
+            r_angular_velocity[2] = aux_orientation[2]/dt
+            r_angular_velocity[3] = aux_orientation[3]/dt
+         
+            r_linear_velocity[1] = aux_position[1]/dt 
+            r_linear_velocity[2] = aux_position[2]/dt 
+            r_linear_velocity[3] = aux_position[3]/dt
+            oldTransformation = transformNow
+            lastTime = timeNow
+	end
+        --r_linear_velocity = simMultiplyVector(transformNow,r_linear_velocity)
         -- ROSing
         local ros_pose = {}
 	ros_pose['header'] = {seq=0,stamp=simROS.getTime(), frame_id="/robot"..modelBaseName}
@@ -145,8 +140,36 @@ if (sim_call_type==sim.childscriptcall_actuation) then
     s=sim.getObjectSizeFactor(objHandle) -- make sure that if we scale the robot during simulation, other values are scaled too!
     v0=0.4*s
     wheelDiameter=0.085*s
-    interWheelDistance=0.254*s
+    interWheelDistance=0.137*s
     noDetectionDistance=0.4*s
-   
+    if simulationIsKinematic then
+        -- Simulation is kinematic
+        p=sim.boolOr32(sim.getModelProperty(objHandle),sim.modelproperty_not_dynamic)
+        sim.setModelProperty(objHandle,p)
+        dt=sim.getSimulationTimeStep()
+        p=sim.getJointPosition(leftJoint)
+        sim.setJointPosition(leftJoint,p+velocityLeft*dt*2/wheelDiameter)
+        p=sim.getJointPosition(rightJoint)
+        sim.setJointPosition(rightJoint,p+velocityRight*dt*2/wheelDiameter)
+        linMov=dt*(velocityLeft+velocityRight)/2.0
+        rotMov=dt*math.atan((velocityRight-velocityLeft)/interWheelDistance)
+        position=sim.getObjectPosition(objHandle,sim.handle_parent)
+        orientation=sim.getObjectOrientation(objHandle,sim.handle_parent)
+        xDir={math.cos(orientation[3]),math.sin(orientation[3]),0.0}
+        position[1]=position[1]+xDir[1]*linMov
+        position[2]=position[2]+xDir[2]*linMov
+        orientation[3]=orientation[3]+rotMov
+        sim.setObjectPosition(objHandle,sim.handle_parent,position)
+        sim.setObjectOrientation(objHandle,sim.handle_parent,orientation)
+    else
+        -- Simulation is dynamic
+        p=sim.boolOr32(sim.getModelProperty(objHandle),sim.modelproperty_not_dynamic)-sim.modelproperty_not_dynamic
+        sim.setModelProperty(objHandle,p)
+        --velocityRight = linVel + rotVel
+        --velocityLeft = linVel - rotVel
+        sim.setJointTargetVelocity(leftJoint,velocityLeft*2/wheelDiameter)
+        sim.setJointTargetVelocity(rightJoint,velocityRight*2/wheelDiameter)
+    end 
+    print(velocityLeft*2/wheelDiameter)
 end 
 
