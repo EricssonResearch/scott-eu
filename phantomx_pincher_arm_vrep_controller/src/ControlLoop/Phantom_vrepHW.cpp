bd87744a8ad238fbc4c13c048d09168f3203337a
amadeu <amadeu.nascimento.junior@ericsson.com>
2018-01-12 10:41:59 -0200

    Arm initial pose the same as vrep

--- phantomx_pincher_arm_vrep_controller/src/ControlLoop/Phantom_vrepHW.cpp
+++ phantomx_pincher_arm_vrep_controller/src/ControlLoop/Phantom_vrepHW.cpp
@@ -16,7 +16,13 @@ std::string Phantom_vrepHW::sm_jointsName[MR_JOINTS_NUM] = {
   "PhantomXPincher_joint3",
   "PhantomXPincher_joint4",
   "PhantomXPincher_gripperCenter_joint",
-  "PhantomXPincher_gripperClose_joint"
+  "PhantomXPincher_gripperClose_joint",
+  "PhantomXPincher_joint1_1",
+  "PhantomXPincher_joint2_1",
+  "PhantomXPincher_joint3_1",
+  "PhantomXPincher_joint4_1",
+  "PhantomXPincher_gripperCenter_joint_1",
+  "PhantomXPincher_gripperClose_joint_1",
 };
 
 
@@ -59,13 +65,18 @@ bool Phantom_vrepHW::init()
 
         m_vrepJointsHandle[i] = vrepJointsHandle;
     }
-
+    read();
+    
+    for (int i=0; i < MR_JOINTS_NUM; i++){
+      m_pos_cmd[i] = m_pos[i];
+    }
     return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void Phantom_vrepHW::registerHardwareInterfaces()
 {
+  ROS_ERROR_STREAM("Join numbers =" << MR_JOINTS_NUM << std::endl);
     for (int i = 0; i < MR_JOINTS_NUM; ++i)
     {
         // Joint state interface.
