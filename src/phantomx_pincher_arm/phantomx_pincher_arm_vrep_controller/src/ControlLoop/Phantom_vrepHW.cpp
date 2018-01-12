226d2218c3da91d33e826fef4731d043037341dc
alberto.hata <alberto.hata@gmail.com>
2018-01-12 12:04:49 -0200

    fixed merge

--- src/phantomx_pincher_arm/phantomx_pincher_arm_vrep_controller/src/ControlLoop/Phantom_vrepHW.cpp
+++ src/phantomx_pincher_arm/phantomx_pincher_arm_vrep_controller/src/ControlLoop/Phantom_vrepHW.cpp
@@ -1,15 +1,15 @@
 #include "Phantom_vrepHW.h"
-
+ 
 #include "../v_repLib.h"
-
+ 
 #include <string>
 #include <iostream>
-
-
+ 
+ 
 namespace MR
 {
-
-
+ 
+ 
 std::string Phantom_vrepHW::sm_jointsName[MR_JOINTS_NUM] = {
   "PhantomXPincher_joint1",
   "PhantomXPincher_joint2",
@@ -24,8 +24,8 @@ std::string Phantom_vrepHW::sm_jointsName[MR_JOINTS_NUM] = {
   "PhantomXPincher_gripperCenter_joint_1",
   "PhantomXPincher_gripperClose_joint_1",
 };
-
-
+ 
+ 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Phantom_vrepHW::Phantom_vrepHW() :
     hardware_interface::RobotHW()
@@ -34,20 +34,20 @@ Phantom_vrepHW::Phantom_vrepHW() :
     for (int i = 0; i < MR_JOINTS_NUM; ++i)
     {
         m_pos_cmd[i] = 0.0;
-        m_vel_cmd[i] = 0.0;	
+        m_vel_cmd[i] = 0.0;     
         m_pos[i] = 0.0;
         m_vel[i] = 0.0;
         m_eff[i] = 0.0;
     }
-
+ 
     // Init and get handles of the joints to control.
     for (int i = 0; i < MR_JOINTS_NUM; ++i)
         m_vrepJointsHandle[i] = -1;
-
+ 
     // Register joint interfaces.
     registerHardwareInterfaces();
 }
-
+ 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 bool Phantom_vrepHW::init()
 {
@@ -55,56 +55,49 @@ bool Phantom_vrepHW::init()
     for (int i = 0; i < MR_JOINTS_NUM; ++i)
     {
         int vrepJointsHandle = simGetObjectHandle(sm_jointsName[i].c_str());
-
+ 
         if (vrepJointsHandle == -1)
         {
             ROS_ERROR_STREAM("MR robot interface not able to get handle for '" << sm_jointsName[i] << "'." << std::endl);
-
+ 
             return false;
         }
-
+ 
         m_vrepJointsHandle[i] = vrepJointsHandle;
     }
-<<<<<<< HEAD
-
-=======
     read();
     
     for (int i=0; i < MR_JOINTS_NUM; i++){
       m_pos_cmd[i] = m_pos[i];
     }
->>>>>>> bd87744a8ad238fbc4c13c048d09168f3203337a
     return true;
 }
-
+ 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void Phantom_vrepHW::registerHardwareInterfaces()
 {
-<<<<<<< HEAD
-=======
   ROS_ERROR_STREAM("Join numbers =" << MR_JOINTS_NUM << std::endl);
->>>>>>> bd87744a8ad238fbc4c13c048d09168f3203337a
     for (int i = 0; i < MR_JOINTS_NUM; ++i)
     {
         // Joint state interface.
         hardware_interface::JointStateHandle jointStateHandle(sm_jointsName[i], &m_pos[i], &m_vel[i], &m_eff[i]);
         m_jointState_interface.registerHandle(jointStateHandle);
-
+ 
         // Joint velocity command interface 
         hardware_interface::JointHandle jointVelocityHandle(jointStateHandle, &m_vel_cmd[i]);
         m_jointVelocity_interface.registerHandle(jointVelocityHandle);
-
-	//Joint position command interface
-	hardware_interface::JointHandle jointPositionHandle(jointStateHandle, &m_pos_cmd[i]);
-	m_jointPosition_interface.registerHandle(jointPositionHandle);
-	  
+ 
+        //Joint position command interface
+        hardware_interface::JointHandle jointPositionHandle(jointStateHandle, &m_pos_cmd[i]);
+        m_jointPosition_interface.registerHandle(jointPositionHandle);
+          
     }
-
+ 
     registerInterface(&m_jointState_interface);
     registerInterface(&m_jointVelocity_interface);
     registerInterface(&m_jointPosition_interface);
 }
-
+ 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 bool Phantom_vrepHW::read()
 {
@@ -113,24 +106,24 @@ bool Phantom_vrepHW::read()
         float pos,
               vel,
               eff;
-
+ 
         if (simGetJointPosition(m_vrepJointsHandle[i], &pos) == -1 ||
             simGetObjectFloatParameter(m_vrepJointsHandle[i], 2012, &vel) == -1 || // Velocity.
             simGetJointForce(m_vrepJointsHandle[i], &eff) == -1)
         {
             ROS_ERROR_STREAM("MR robot interface not able to get state for '" << sm_jointsName[i] << "'." << std::endl);
-
+ 
             return false;
         }
-
+ 
         m_pos[i] = pos;
         m_vel[i] = vel;
         m_eff[i] = eff;
     }
-
+ 
     return true;
 }
-
+ 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 bool Phantom_vrepHW::write()
 {
@@ -138,18 +131,19 @@ bool Phantom_vrepHW::write()
       //====== Velocity Control =========
       
       /*if (simSetJointTargetVelocity(m_vrepJointsHandle[i], m_vel_cmd[i]) == -1) {
-	ROS_ERROR_STREAM("MR robot interface not able to get state for '" << sm_jointsName[i] << "'." << std::endl);
-	return false;
+        ROS_ERROR_STREAM("MR robot interface not able to get state for '" << sm_jointsName[i] << "'." << std::endl);
+        return false;
       }
       */
-
+ 
       //===== Position Control =======
       if (simSetJointTargetPosition(m_vrepJointsHandle[i], m_pos_cmd[i]) == -1) {
-	ROS_ERROR_STREAM("MR robot interface not able to get state for '" << sm_jointsName[i] << "'." << std::endl);
-	return false;
-      }	
+        ROS_ERROR_STREAM("MR robot interface not able to get state for '" << sm_jointsName[i] << "'." << std::endl);
+        return false;
+      } 
     }
     return true;
 }
-
+ 
 } // namespace MR.
+ 
