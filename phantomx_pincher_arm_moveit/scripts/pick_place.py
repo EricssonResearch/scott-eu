5d3f35660dc335f23a4007247fb8adb35f23b9d1
amadeu <amadeu.nascimento.junior@ericsson.com>
2018-06-20 17:06:44 -0300

    Removed uneeded lines

--- phantomx_pincher_arm_moveit/scripts/pick_place.py
+++ phantomx_pincher_arm_moveit/scripts/pick_place.py
@@ -27,12 +27,11 @@ def closeGripper():
     print posture
     return posture
     
-def try_target(target, arm, attemps = 10):
+def ef_pose(target, arm, attemps = 10):
     d = pow(pow(target[0], 2) + pow(target[1], 2), 0.5)
     if d > 3.0:
         print("Too far. Out of reach")
         return None
-    print("Distance: ", d)
     rp = np.pi/2.0 - np.arcsin((d-0.1)/.205)
     ry = np.arctan2(target[1], target[0])
 
@@ -40,11 +39,9 @@ def try_target(target, arm, attemps = 10):
     again = True
     while (again and attemp < 10):
         again = False
-        print attemp
 
         rp_a = attemp * ((0.05 + 0.05)*np.random.ranf() - 0.05) + rp
         ry_a = attemp * ((0.05 + 0.05)*np.random.ranf() - 0.05) + ry
-
         q = tf.transformations.quaternion_from_euler(0, rp_a, ry_a)
         print [0, rp_a, ry_a]
 
@@ -59,8 +56,6 @@ def try_target(target, arm, attemps = 10):
 
         target[2] += np.abs(np.cos(rp))/250.0
 
-        print p
-
         arm.set_pose_target(p)
         planed = arm.plan()
         if len(planed.joint_trajectory.points) == 0:
@@ -71,53 +66,10 @@ def try_target(target, arm, attemps = 10):
         return None
     return planed
 
-class Arm():
-    def __init__(self):
-        self.stoped = True
-        self.count_stp = 0
-        #rospy.Subscriber('phantom_controller/state',
-        #                 JointTrajectoryControllerState,
-        #                 self.checkStopedArm)
-        #rospy.Subscriber('joint_states',
-        #                 JointState,
-        #                 self.checkStopedArm)
-
-    def checkStopedArm(self, state):
-        '''
-        Simple function to check if the arm is stopped for a while
-        '''
-        #vel = np.abs(np.asarray(state.actual.velocities))
-        vel = np.abs(np.asarray(state.velocity))
-        print vel
-        vel = sum(vel > 0.005)
-        print 'velocities'
-        print vel
-        print 'stop'
-        print self.stoped
-        if vel > 0:
-            self.count_stp = 0
-            self.stoped = False
-            return 
-        else:
-            self.count_stp += 1
-            time.sleep(0.1)
-        if self.count_stp > 10:
-            self.stoped = True
-
-    def isInPosition(self, state, target):
-        state = np.asarray(state)
-        target = np.asarray(target)
-        diff = abs(target-state)
-        tol = 1/180 * np.pi
-        diff = sum(diff > tol)
-        if diff > 0:
-            return True
-        return False
 
 if __name__ == '__main__':
     roscpp_initialize(sys.argv)
     rospy.init_node('moveit_py_demo', anonymous=True)
-    arm = Arm()
     robot = RobotCommander()
     scene = PlanningSceneInterface()
 
@@ -128,7 +80,7 @@ if __name__ == '__main__':
     rospy.sleep(2)
     scene.remove_world_object("base")
     scene.remove_attached_object("gripper_link", "box")
-    print robot.get_planning_frame()
+
     p = geometry_msgs.msg.PoseStamped()
     p.header.frame_id = "base_link"
     p.pose.position.x = 0.165
@@ -148,31 +100,14 @@ if __name__ == '__main__':
     scene.add_box("box", p, [0.03, 0.03, 0.03])
     rospy.sleep(1)
 
-    #exit()
-
     robot = RobotCommander()
-    print "Current state:"    
-    robot.get_current_state()
     robot.pincher_arm.set_start_state(RobotState())
 
-    print "Arm Joints:"
-    print robot.pincher_arm.get_joints()
-
-    print "Arm Tip:"
-    print robot.pincher_arm.get_end_effector_link()
-
     #robot.pincher_arm.set_planner_id('RRTConnectkConfigDefault')
-
     robot.pincher_arm.set_num_planning_attempts(10000)
     robot.pincher_arm.set_planning_time(5)
-    print "Tolerances"
     robot.pincher_arm.set_goal_position_tolerance(0.01)
     robot.pincher_arm.set_goal_orientation_tolerance(0.5)
-    print robot.pincher_arm.get_goal_position_tolerance()
-    print robot.pincher_arm.get_goal_orientation_tolerance()
-
-    pose_now = robot.pincher_arm.get_current_pose()
-    print pose_now
 
     # -----------------------------
     #  Abre o gripper
@@ -188,12 +123,10 @@ if __name__ == '__main__':
     # -----------------------------
     #  Vai para algum lugar
     #------------------------------
-    #0.19169 0.05708 0.13376 -0.02705 0.1856 0.1416 0.9719
     robot.get_current_state()
     robot.pincher_arm.set_start_state(RobotState())
     target = [0.17, 0.005, 0.019]
-    #target = [0.17, 0.05, 0.025]
-    plan = try_target(target, robot.pincher_arm)
+    plan = ef_pose(target, robot.pincher_arm)
 
 
     if plan is not None:
@@ -203,44 +136,10 @@ if __name__ == '__main__':
         print('No trajectory found')
         exit()
 
-    count = 0
-    while not (arm.isInPosition(robot.pincher_arm.get_current_joint_values(),
-                                plan.joint_trajectory.points[-1].positions)):
-        print 'Moving'
-        count = count + 1
-        if (count % 100 == 0):
-            print robot.pincher_arm.get_current_joint_values()
-            print plan.joint_trajectory.points[-1].positions
     rospy.sleep(5)
-    # -----------------------------
-    #  Vai para algum lugar
-    #------------------------------
-    #0.19169 0.05708 0.13376 -0.02705 0.1856 0.1416 0.9719
-    '''
-    robot.get_current_state()
-    robot.pincher_arm.set_start_state(RobotState())
-    target = [0.17, 0, 0.019]
-    #target = [0.17, 0.05, 0.025]
-    plan = try_target(target, robot.pincher_arm)
-
-    if plan is not None:
-        robot.pincher_arm.execute(plan)
-        print(plan)
-    else:
-        print('No trajectory found')
-        exit()
-
-    while not (arm.isInPosition(robot.pincher_arm.get_current_joint_values(),
-                                plan.joint_trajectory.points[-1].positions)):
-        print 'Moving'
-        count = count + 1
-        if (count % 100 == 0):
-            print robot.pincher_arm.get_current_joint_values()
-            print plan.joint_trajectory.points[-1].positions
-
-    '''
     scene.remove_world_object("box")
     rospy.sleep(1)
+
     # -----------------------------
     # Fecha o gripper
     #------------------------------
@@ -249,9 +148,9 @@ if __name__ == '__main__':
     fechado = closeGripper()
     robot.pincher_gripper.set_joint_value_target(fechado)
     gplan = robot.pincher_gripper.plan()
-    print gplan
     robot.pincher_gripper.execute(gplan)
 
+    
     rospy.sleep(1)
     scene.attach_box("gripper_link", "box", p, [0.03, 0.03, 0.03])
     rospy.sleep(1)
@@ -259,11 +158,10 @@ if __name__ == '__main__':
     # -----------------------------
     #  Vai para algum lugar
     #------------------------------
-    #0.19169 0.05708 0.13376 -0.02705 0.1856 0.1416 0.9719
     robot.get_current_state()
     robot.pincher_arm.set_start_state(RobotState())
     target = [0.17, 0.10, 0.028]
-    plan = try_target(target, robot.pincher_arm)
+    plan = ef_pose(target, robot.pincher_arm)
     
     if plan is not None:
         robot.pincher_arm.execute(plan)
@@ -271,15 +169,6 @@ if __name__ == '__main__':
     else:
         print('No trajectory found')
         exit()
-
-    count = 0
-    while not (arm.isInPosition(robot.pincher_arm.get_current_joint_values(),
-                                plan.joint_trajectory.points[-1].positions)):
-        print 'Moving'
-        count = count + 1
-        if (count % 100 == 0):
-            print robot.pincher_arm.get_current_joint_values()
-            print plan.joint_trajectory.points[-1].positions
 
     rospy.sleep(5)
     scene.remove_attached_object("gripper_link", "box")
@@ -294,5 +183,4 @@ if __name__ == '__main__':
     fechado = openGripper()
     robot.pincher_gripper.set_joint_value_target(fechado)
     gplan = robot.pincher_gripper.plan()
-    print gplan
     robot.pincher_gripper.execute(gplan)
