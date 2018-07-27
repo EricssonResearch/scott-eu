6edf684cca581534008b7352b427994f0a254303
Klaus <klaus.raizer@ericsson.com>
2018-07-27 16:35:35 -0300

    Created dummy list of tasks and fed it to soar's input link via ROS topic

--- simulation-ros/src/turtlebot2i/turtlebot2i_soar/src/soar_robot.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_soar/src/soar_robot.py
@@ -20,14 +20,13 @@ sys.path.append(LIB_PATH)
 
 import Python_sml_ClientInterface as sml # Python interface to SOAR
 import rospy # ROS library
-
+import json
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 
 import message_filters
 from std_msgs.msg import String
-
-
+from geometry_msgs.msg import Twist
 
 """ Callback functions to help us see what is happening inside agent's mind"""
 def register_print_callback(kernel, agent, function, user_data=None):
@@ -69,6 +68,9 @@ class ToyEnv(object):
 pub=None #pub should be visible by main and callbacks
 sub=None #sub should be visible by main and callbacks
 
+vel_output_pub=None
+vel_msg=None
+
 ## SOAR variables    #TODO is there a better way of doing this?
 kernel=None
 agent=None
@@ -78,6 +80,7 @@ a_value=None
 b_value=None
 output_link=None
 
+tasks_wme=None
 
 seq_wme=None
 secs_wme=None
@@ -104,25 +107,55 @@ yta_wme=None
 zta_wme=None
 
 
-def topic_callback(odom_data, scan_data,data):
+def topic_callback(odom_data, scan_data,tasks_data,data):
 
-    rospy.loginfo('odom_stamp: ' + str(odom_data.header.stamp.to_sec()) + ' ; scan_stamp: ' + str(scan_data.header.stamp.to_sec())+ ' ; data: ' + str(data))
 
-   
+    input_link=agent.GetInputLink()
+
+    rospy.loginfo('odom_stamp: ' + str(odom_data.header.stamp.to_sec()) + ' ; scan_stamp: ' + str(scan_data.header.stamp.to_sec())+ ' ; tasks_data: ' + str(tasks_data)+ ' ; data: ' + str(data))
+
+    
+
+ 
+    
+
+#    for i in range(len(tasks_data_json['plan'])):
+#        print('=====<<<<<< tasks_data_json: ', tasks_data_json['plan'][i])  
+#        tasks_data_json['plan'][i]
+
+
+#        tasks_wme_list.append(tasks_data_json['plan'][i])
+
+#    print("tasks_wme_list: ",tasks_wme_list)
+
+
+   # if(tasks_wme!=None):
+   #     tasks_wme=agent.DestroyWME(tasks_wme)
+   # 
+   # tasks_wme=agent.CreateIdWME(input_link,"tasks")
+
+#    tasks_wme = agent.CreateIdWME(input_link,"tasks")
+#    task_wme=agent.CreateIdWME(tasks_wme,"next_task")
+#    task_wme_name=agent.CreateStringWME(task_wme,"name","move") 
+#    task_wme_waypoint=agent.CreateStringWME(task_wme,"waypoint","Waypoint_SH#1") 
+
+
+    
+
 
     #Do something with data
     dict=eval(data.data)
+
     ## Cognitive cycle goes here
 
     a_value_temp=float(dict['a_value'])
     b_value_temp=float(dict['b_value'])
 
-    # 2) push senses to soar
+    ## 2) push senses to soar
     a_value.Update(a_value_temp)
     b_value.Update(b_value_temp)
 
     #Odometer
-    print("odom_data.pose.pose.position.x: ",odom_data.pose.pose.position.x)
     xp_wme.Update(float(odom_data.pose.pose.position.x))
     yp_wme.Update(float(odom_data.pose.pose.position.y))
     zp_wme.Update(float(odom_data.pose.pose.position.z))
@@ -140,17 +173,42 @@ def topic_callback(odom_data, scan_data,data):
     yta_wme.Update(float(odom_data.twist.twist.angular.y))
     zta_wme.Update(float(odom_data.twist.twist.angular.z))
 
+    #Tasks List
+    global tasks_wme
+    tasks_data_json = json.loads(str(tasks_data.data))
+    if(tasks_wme!=None):
+        print("DestroyWME")
+        tasks_wme=agent.DestroyWME(tasks_wme)
+
+    tasks_wme = agent.CreateIdWME(input_link,"tasks")
+
+    tasks_wme_list=[]
+    previous_task_wme=tasks_wme
+
+    for i in range(len(tasks_data_json['plan'])):
+        task_wme=agent.CreateIdWME(previous_task_wme,"next_task")
+        action_name=str(tasks_data_json['plan'][i][0])
+        agent.CreateStringWME(task_wme,"name",action_name) 
+        if(action_name=='move'):
+            agent.CreateStringWME(task_wme,"waypoint",str(tasks_data_json['plan'][i][2])) 
+        elif(action_name=='pick'):
+            agent.CreateStringWME(task_wme,"shelf",str(tasks_data_json['plan'][i][2])) 
+            agent.CreateStringWME(task_wme,"product",str(tasks_data_json['plan'][i][3]))
+        elif(action_name=='drop'):
+            agent.CreateStringWME(task_wme,"conveyor_belt",str(tasks_data_json['plan'][i][2])) 
+            agent.CreateStringWME(task_wme,"product",str(tasks_data_json['plan'][i][3])) 
+        else:
+            print('unknown action: ',action_name)
+        previous_task_wme=task_wme
 
 
 
-
-
-    # 3) make soar think about it
+    ## 3) make soar think about it
     result=0
     #run_result=agent.RunSelf(1)    #Run agent for one step (should run until output?)
     run_result=agent.RunSelfTilOutput() #TODO see why so many substates are being created
     
-    # 4) get results from soar
+    ## 4) get results from soar
     output_link=agent.GetOutputLink()## returns an Identifier
     if output_link!= None:
         result_output_wme = output_link.FindByAttribute("result", 0) # returns a WMElement of the form (<output_link> ^result <val>)
@@ -158,15 +216,48 @@ def topic_callback(odom_data, scan_data,data):
         if result_output_wme != None:
             result = float(result_output_wme.GetValueAsString())
 
-    #5) send result to environment
+    ## 5) send result to environment
     te.set_actuators(result) 
     rospy.loginfo("output result log: "+str(result))
     pub.publish("output result topic: "+str(result)) # Here goes the output
 
+    vel_msg=Twist()
+    vel_msg.linear.x=0.0
+    vel_msg.linear.y=0.0
+    vel_msg.linear.z=0.0
+    vel_msg.angular.x=0.0
+    vel_msg.angular.y=0.0
+    vel_msg.angular.z=0.0
+    vel_output_pub.publish(vel_msg)
+    print("vel_msg: ",vel_msg)
+
+
     cli(agent) #open client to interact with agent
 
-""" Main program """
+
+""" =============================================================== """
+""" === Main program                                            === """
+""" =============================================================== """
 if __name__ == "__main__":   
+
+
+
+    json_string = """
+    {
+        "id": "xyz",
+        "init": "initURI",
+        "goal": "finalURI",
+        "plan":[
+                    ["move", "youBot#0", "Waypoint_SH#1"],
+                    ["pick", "youBot#0", "Shelf#1", "productRed"],
+                    ["move", "youBot#0", "Waypoint_CB"],
+                    ["drop", "youBot#0", "ConveyorBelt", "productRed"]
+                    ]
+    }
+    """
+    temp_tasks_dummy_data = json.loads(json_string)
+    tasks_dummy_data=str(json.dumps(temp_tasks_dummy_data)) #makes sure it is a valid json when converting to str
+   
 
 
 #-- SOAR AGENT INITIALIZATION-----------------------------------
@@ -188,7 +279,7 @@ if __name__ == "__main__":
     a_value=agent.CreateFloatWME(input_link, "a", -1.0)
     b_value=agent.CreateFloatWME(input_link, "b", -1.0)
 
-    ##TODO Create input structure for odometer #TODO How about doing this automatically based on the topic's structure?
+    #TODO How about doing this automatically based on the topic's structure?
     odom_wme=agent.CreateIdWME(input_link,"odom")
 
     header_wme=agent.CreateIdWME(odom_wme,"header")  
@@ -231,7 +322,16 @@ if __name__ == "__main__":
     yta_wme=agent.CreateFloatWME(twist_angular_wme,"y",-1.0)
     zta_wme=agent.CreateFloatWME(twist_angular_wme,"z",-1.0)
 
+
     
+#    tasks_wme=agent.CreateIdWME(input_link,"tasks")
+#    task_wme=agent.CreateIdWME(tasks_wme,"next_task")
+#    task_wme_name=agent.CreateStringWME(task_wme,"name","move") 
+#    task_wme_waypoint=agent.CreateStringWME(task_wme,"waypoint","Waypoint_SH#1") 
+
+
+
+
 
     #Get output link
     output_link=agent.GetOutputLink()
@@ -248,22 +348,23 @@ if __name__ == "__main__":
     sub=message_filters.Subscriber("/turtlebot2i/soar_sub_topic", String)
     odom_sub = message_filters.Subscriber('/turtlebot2i/odom', Odometry)
     scan_sub = message_filters.Subscriber('/turtlebot2i/lidar/scan', LaserScan)
+    tasks_sub = message_filters.Subscriber('/turtlebot2i/tasks', String)
 
     # Make the topics sync through ApproximateTimeSynchronizer with 0.1s of tolerance
-    ts =  message_filters.ApproximateTimeSynchronizer([odom_sub, scan_sub,sub], 10, 0.1, allow_headerless=True)
+    ts =  message_filters.ApproximateTimeSynchronizer([odom_sub, scan_sub,tasks_sub,sub], 10, 0.1, allow_headerless=True)
 
     # Associate the synchronizer with a callback
     ts.registerCallback(topic_callback)
 
     ## PUBLISHERS
     pub=rospy.Publisher("soar_pub_topic", String, queue_size=10)
-    dummy_pub=rospy.Publisher("/turtlebot2i/soar_sub_topic",String, queue_size=10) #used for inputing debug data to soar_sub_topic
-    #tasks_list_topic=rospy.Subscriber("soar_tasks_list_topic", String, topic_callback)
-#    dummy_tasks_list_topic=rospy.Publisher("soar_tasks_list_topic",String, queue_size=10) #used for inputing debug data to soar_tasks_list_topic
+    vel_output_pub=rospy.Publisher("/turtlebot2i/commands/velocity",Twist, queue_size=10) #velocity output publisher
+    vel_msg=Twist()
 
+    dummy_pub=rospy.Publisher("/turtlebot2i/soar_sub_topic",String, queue_size=10) #dummy pubs are used for inputing debug data to input topics
+    dummy_tasks_pub=rospy.Publisher("/turtlebot2i/tasks", String, queue_size=10)
 
-
-
+ 
 
 #    sub=rospy.Subscriber("soar_sub_topic", String, topic_callback)
 #    pub=rospy.Publisher("soar_pub_topic",String, queue_size=10)
@@ -290,6 +391,7 @@ if __name__ == "__main__":
 
         dummy_pub.publish(str(new_input)) 
 #        rospy.loginfo("Log new_input: "+ str(new_input))
+        dummy_tasks_pub.publish(str(tasks_dummy_data))
        
 
        
@@ -391,4 +493,43 @@ twist:
 ---
 
 """
+
+
+"""
+^Cklaus@klaus-VirtualBox:~$ rostopic pub /turtlebot2i/commands/velocity geometrysgs/Twist "linear:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0" 
+publishing and latching message. Press ctrl-C to terminate
+
+"""
+
+"""
+Planning service output is a JSON object which looks like
+{
+    "id": "xyz",
+    "init": "initURI",
+    "goal": "finalURI",
+    "plan":[
+                ["move", "youBot#0", "Waypoint_SH#1"],
+                ["pick", "youBot#0", "Shelf#1", "productRed"],
+                ["pick", "youBot#0", "Shelf#1", "productGreen"],
+                ["move", "youBot#0", "Waypoint_CB"],
+                ["drop", "youBot#0", "ConveyorBelt", "productRed"],
+                ["move", "youBot#0", "Waypoint_CB#0"],
+                ["drop", "youBot#0", "ConveyorBelt#0", "productGreen"],
+                ["move", "youBot#1", "Waypoint_SH#0"],
+                ["pick", "youBot#1", "Shelf#0", "productYellow"],
+                ["move", "youBot#1", "Waypoint_CB#0"],
+                ["drop", "youBot#1", "ConveyorBelt#0", "productYellow"],
+                ["move", "youBot#1", "Waypoint_CB#1"],
+                ["drop", "youBot#1", "ConveyorBelt#1", "productGreen"]
+                ]
+}
+"""
+
     
