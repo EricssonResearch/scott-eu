646d0ca0230a885caaa513187bd81ef90375c863
Leonid Mokrushin <leonid.mokrushin@ericsson.com>
2018-08-02 11:52:55 +0200

    created arbitrary resource creation factory for testing visualization

--- planner_reasoner/applications/planner_reasoner_server.pl
+++ planner_reasoner/applications/planner_reasoner_server.pl
@@ -22,10 +22,13 @@ limitations under the License.
 :- use_module(library(pddl_generator)).
 :- use_module(library(metric_ff)).
 :- use_module(library(validate)).
+:- use_module(library(oslc)).
+:- use_module(library(oslc_error)).
 
 :- oslc_post(planner:pddlCreationFactory, pddl_creation_factory).
 :- oslc_post(planner:planCreationFactory, plan_creation_factory).
 :- oslc_post(planner:validatedPlanCreationFactory, validated_plan_creation_factory).
+:- oslc_post(planner:resourceCreationFactory, resource_creation_factory).
 
 oslc_dispatch:serializer(text/'x-pddl', turtle).
 
@@ -72,3 +75,45 @@ validated_plan_creation_factory(Context) :-
     ; throw(response(400))
     ))
   )).
+
+resource_creation_factory(Context) :-
+  post_resource(Context, rdf(remotely_created_resources)).
+
+post_resource(Context, Sink) :-
+  catch((
+      setting(oslc_prolog_server:prefix_path, PrefixPath),
+      memberchk(protocol(Protocol), Context.request),
+      memberchk(host(Host), Context.request),
+      once((
+        memberchk(port(Port), Context.request)
+      ; Port = 80
+      )),
+      ( Port == 80
+      -> Authority = Host
+      ; format(atom(Authority), '~w:~w', [Host, Port])
+      ),
+      post_resource0(Context.iri, rdf(Context.graph_in), Sink, NewResource),
+      rdf_global_id(Prefix:Name, NewResource),
+      format(atom(NewPath), '~w~w/~w', [PrefixPath, Prefix, Name]),
+      uri_components(Location, uri_components(Protocol, Authority, NewPath, _, _)),
+      response(201, ['Location'(Location)]) % created
+    ),
+    oslc_error(Message),
+    throw(response(400, Message)) % bad request
+  ).
+
+post_resource0(IRI, Source, Sink, NewResource) :-
+  once((
+    ground(Source)
+  ; oslc_error('Missing resource in POST request to [~w]', [IRI])
+  )),
+  once((
+    unmarshal_property(NewResource, rdf:type, _, _, Source),
+    \+ rdf_is_bnode(NewResource)
+  ; oslc_error('Missing resource type in POST request to [~w]', [IRI])
+  )),
+  once((
+    \+ unmarshal_property(NewResource, _, _, _, Sink)
+  ; oslc_error('Resource [~w] already exists (use PUT to update it instead)', [NewResource])
+  )),
+  copy_resource(NewResource, NewResource, Source, Sink, []).
