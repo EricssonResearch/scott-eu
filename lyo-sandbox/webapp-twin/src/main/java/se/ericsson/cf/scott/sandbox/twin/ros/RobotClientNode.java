6758a62ac9d7a27dfcf05b959de34817f51308ef
eriasoa <ricardo.s.souza@ericsson.com>
2018-08-14 22:59:03 -0300

    Adding publishers to twin and rosmaster container

--- lyo-sandbox/webapp-twin/src/main/java/se/ericsson/cf/scott/sandbox/twin/ros/RobotClientNode.java
+++ lyo-sandbox/webapp-twin/src/main/java/se/ericsson/cf/scott/sandbox/twin/ros/RobotClientNode.java
@@ -6,9 +6,12 @@ import org.ros.exception.ServiceNotFoundException;
 import org.ros.message.Time;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
+import org.ros.node.Node;
 import org.ros.node.ConnectedNode;
 import org.ros.node.topic.Subscriber;
+import org.ros.node.topic.Publisher;
 import org.ros.message.MessageListener;
+import org.ros.concurrent.CancellableLoop;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turtlebot2i_warehouse.TaskRequest;
@@ -17,7 +20,8 @@ import turtlebot2i_warehouse.TaskRequestResponse;
 
 public class RobotClientNode extends AbstractNodeMain {
 
-    private final static Logger log           = LoggerFactory.getLogger(RobotClientNode.class);
+    private final static Logger log =
+	LoggerFactory.getLogger(RobotClientNode.class);
     private final static String TASK_SVC_NAME = "TaskRequestService";
 
     @Override
@@ -30,64 +34,96 @@ public class RobotClientNode extends AbstractNodeMain {
             log.info("RobotClientNode started");
 
 	    /*
-	     *  EVENTS
+	     * Subscribers
 	     */
 
+	     //  EVENTS
+
 	    // Bumper
-	    Subscriber<kobuki_msgs.BumperEvent> bumperEventSubscriber = connectedNode.newSubscriber("/turtlebot2i/events/bumper", kobuki_msgs.BumperEvent._TYPE);
+	    final Subscriber<kobuki_msgs.BumperEvent> bumperEventSubscriber =
+		connectedNode.newSubscriber("/turtlebot2i/events/bumper",
+					    kobuki_msgs.BumperEvent._TYPE);
 	    bumperEventSubscriber.addMessageListener(new MessageListener<kobuki_msgs.BumperEvent>() {
+		    private int count = 0;
 		    @Override
 		    public void onNewMessage(kobuki_msgs.BumperEvent message) {
-			log.info("Bumper Event: \"" + message.getState() + "\"");
+			count++;
+			if(count%5 == 0){
+			    log.info("Bumper Event: \"" + message.getState() + "\"");
+			}
 		    }
 		});
 
 	    // Cliff
-	    Subscriber<kobuki_msgs.CliffEvent> cliffEventSubscriber = connectedNode.newSubscriber("/turtlebot2i/events/cliff", kobuki_msgs.CliffEvent._TYPE);
+	    final Subscriber<kobuki_msgs.CliffEvent> cliffEventSubscriber =
+		connectedNode.newSubscriber("/turtlebot2i/events/cliff",
+					    kobuki_msgs.CliffEvent._TYPE);
 	    cliffEventSubscriber.addMessageListener(new MessageListener<kobuki_msgs.CliffEvent>() {
+		    private int count = 0;
 		    @Override
 		    public void onNewMessage(kobuki_msgs.CliffEvent message) {
-			log.info("Cliff Event: \"" + message.getState() + "\"");
+			count++;
+			if(count%5 == 0){
+			    log.info("Cliff Event: \"" + message.getState() + "\"");
+			}
 		    }
 		});
 
 	    // Wheel Drop
-	    Subscriber<kobuki_msgs.WheelDropEvent> wDropEventSubscriber = connectedNode.newSubscriber("/turtlebot2i/events/wheel_drop", kobuki_msgs.WheelDropEvent._TYPE);
+	    final Subscriber<kobuki_msgs.WheelDropEvent> wDropEventSubscriber =
+		connectedNode.newSubscriber("/turtlebot2i/events/wheel_drop",
+					    kobuki_msgs.WheelDropEvent._TYPE);
 	    wDropEventSubscriber.addMessageListener(new MessageListener<kobuki_msgs.WheelDropEvent>() {
+		    private int count = 0;
 		    @Override
 		    public void onNewMessage(kobuki_msgs.WheelDropEvent message) {
-			log.info("Wheel Drop Event: \"" + message.getState() + "\"");
+			count++;
+			if(count%5 == 0){
+			    log.info("Wheel Drop Event: \"" + message.getState() + "\"");
+			}
 		    }
 		});
 
-	    /*
-	     *  SENSORS
-	     */
+	    
+	    //  SENSORS
 
 	    // Odometry
-	    Subscriber<nav_msgs.Odometry> odomSubscriber = connectedNode.newSubscriber("/turtlebot2i/odom", nav_msgs.Odometry._TYPE);
+	    final Subscriber<nav_msgs.Odometry> odomSubscriber =
+		connectedNode.newSubscriber("/turtlebot2i/odom",
+					    nav_msgs.Odometry._TYPE);
 	    odomSubscriber.addMessageListener(new MessageListener<nav_msgs.Odometry>() {
+		    private int count = 0;
 		    @Override
 		    public void onNewMessage(nav_msgs.Odometry message) {
-			System.out.println("Odometry: \"" + message.getPose().getPose() + "\"");
+			//			count++;
+			//if(count%5 == 0) {
+			    log.info("Odometry: \"" + message.getPose().getPose() + "\"");
+			    //}
 		    }
 		});
 
 	    // Dock IR
-	    /*	    Subscriber<kobuki_msgs.DockInfraRed> dockIRSubscriber = connectedNode.newSubscriber("/turtlebot2i/sensors/dock_ir", kobuki_msgs.DockInfraRed._TYPE);
+	    final Subscriber<kobuki_msgs.DockInfraRed> dockIRSubscriber =
+		connectedNode.newSubscriber("/turtlebot2i/sensors/dock_ir",
+					    kobuki_msgs.DockInfraRed._TYPE);
 	    dockIRSubscriber.addMessageListener(new MessageListener<kobuki_msgs.DockInfraRed>() {
+		    private int count = 0;
 		    @Override
 		    public void onNewMessage(kobuki_msgs.DockInfraRed message) {
-			log.info("Dock IR Sensor: \"" + message.getData() + "\"");
+			count++;
+			if(count%5 == 0){
+			    log.info("Dock IR Sensor: \"" + message + "\"");
+			}
 		    }
-		    });*/
+		});
 
-	    /*
-	     *  ACTIONS
-	     */
+	    
+	    //  ACTIONS
 
 	    // Velocity
-	    Subscriber<geometry_msgs.Twist> velSubscriber = connectedNode.newSubscriber("/turtlebot2i/commands/velocity", geometry_msgs.Twist._TYPE);
+	    /*	    final Subscriber<geometry_msgs.Twist> velSubscriber =
+		connectedNode.newSubscriber("/turtlebot2i/commands/velocity",
+					    geometry_msgs.Twist._TYPE);
 	    velSubscriber.addMessageListener(new MessageListener<geometry_msgs.Twist>() {
 		    @Override
 		    public void onNewMessage(geometry_msgs.Twist message) {
@@ -96,16 +132,49 @@ public class RobotClientNode extends AbstractNodeMain {
 		});
 
 	    // Motor Power
-	    Subscriber<kobuki_msgs.MotorPower> motorPowerSubscriber = connectedNode.newSubscriber("/turtlebot2i/commands/motor_power", kobuki_msgs.MotorPower._TYPE);
+	    final Subscriber<kobuki_msgs.MotorPower> motorPowerSubscriber =
+		connectedNode.newSubscriber("/turtlebot2i/commands/motor_power",
+					    kobuki_msgs.MotorPower._TYPE);
 	    motorPowerSubscriber.addMessageListener(new MessageListener<kobuki_msgs.MotorPower>() {
 		    @Override
 		    public void onNewMessage(kobuki_msgs.MotorPower message) {
 			log.info("Motor: \"" + message.getState() + "\"");
 		    }
-		});
+		    });*/
 
+	    /*
+	     * Publishers
+	     */
+
+	    // velocity
+	    	    final Publisher<geometry_msgs.Twist> velPub =
+		connectedNode.newPublisher("/turtlebot2i/commands/velocity",
+					   geometry_msgs.Twist._TYPE);
 	    
-	    
+	    connectedNode.executeCancellableLoop(new CancellableLoop() {
+		    private int sequenceNumber;
+
+		    @Override
+		    protected void setup() {
+			sequenceNumber = 0;
+		    }
+
+		    @Override
+		    protected void loop() throws InterruptedException {
+			geometry_msgs.Twist twt = velPub.newMessage();
+			twt.getLinear().setX(2);
+			velPub.publish(twt);
+			sequenceNumber++;
+			Thread.sleep(5000);
+			twt.getLinear().setX(0);
+			twt.getAngular().setZ(1);
+			velPub.publish(twt);
+			Thread.sleep(3000);
+			twt.getLinear().setX(0);
+			twt.getAngular().setZ(0);
+			velPub.publish(twt);
+		    }
+		});
 	    /*serviceClient = connectedNode.newServiceClient(TASK_SVC_NAME, TaskRequest._TYPE);
             final TaskRequestRequest request = serviceClient.newMessage();
             request.setRobot("RobotA");
@@ -131,5 +200,17 @@ public class RobotClientNode extends AbstractNodeMain {
 	    /*} catch (ServiceNotFoundException e) {
             log.error("ROS exception", e);
 	    }*/
+    }
+
+    @Override
+    public void onShutdown(Node node) {
+    }
+
+    @Override
+    public void onShutdownComplete(Node node) {
+    }
+
+    @Override
+    public void onError(Node node, Throwable throwable) {
     }
 }
