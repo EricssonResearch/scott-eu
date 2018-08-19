6a058c1953e45313568fc90eff55c36d1370e188
Enyu Cao <caoe@kth.se>
2018-08-19 10:51:26 +0200

    change talker topic: Doesn't work

--- simulation-ros/src/turtlebot2i/turtlebot2i_safety/developing_scripts/navi_goal_talker.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_safety/developing_scripts/navi_goal_talker.py
@@ -1,38 +1,51 @@
 #!/usr/bin/env python
 import rospy
-from nav_msgs.msg import MoveBaseGoal
-from geometry_msgs.msg import PoseStamped
+import move_base_msgs.msg
+from move_base_msgs.msg import MoveBaseActionGoal,MoveBaseGoal
+from actionlib_msgs.msg import GoalID
+import geometry_msgs.msg
+from geometry_msgs.msg import PoseStamped#,Pose,Point,Quaternion
 import std_msgs.msg
+
 # Should I import the child message file?
+#--Yes, if you need XXX=msg_name()
 def talker(): 
-    pub = rospy.Publisher('/turtlebot2i/move_base_simple/goal', MoveBaseGoal, queue_size=10)
+    #pub = rospy.Publisher('/turtlebot2i/move_base_simple/goal',PoseStamped, queue_size=10) #doesn't work
+    pub = rospy.Publisher('/turtlebot2i/move_base/goal',MoveBaseActionGoal, queue_size=10) 
+    # Other type:move_base_msgs/MoveBaseActionGoal
+
     rospy.init_node('navi_goal_talker', anonymous=True)
     rate = rospy.Rate(10) # 10 Hz
+        
+    goal_message=MoveBaseActionGoal()    
+    goal_message.header=std_msgs.msg.Header()
+    goal_message.goal_id=GoalID() #empty
+    goal_message.goal=MoveBaseGoal()
+    goal_message.goal.target_pose=PoseStamped()
+    goal_message.goal.target_pose.header = std_msgs.msg.Header()
+    goal_message.goal.target_pose.pose=geometry_msgs.msg.Pose()
 
-    rospy.loginfo("Publish a navi goal")
-    
-    goal_message = MoveBaseGoal()
-    goal_message.target_pose=PoseStamped()
-    goal_message.target_pose.header = std_msgs.msg.Header()
-    
-    goal_message.target_pose.header
+    goal_message.header.stamp = rospy.Time.now()
+    #target_pose.header.seq=5
+    goal_message.goal.target_pose.header.stamp = rospy.Time.now()
+    goal_message.goal.target_pose.header.frame_id="base_link"#"map"
+    goal_message.goal.target_pose.pose.position.x=1#4.8#-0.8  #4.8
+    goal_message.goal.target_pose.pose.position.y=0#-4.0 #0.5  
+    goal_message.goal.target_pose.pose.position.z=0.0
 
-    
+    goal_message.goal.target_pose.pose.orientation.x=0.0
+    goal_message.goal.target_pose.pose.orientation.y=0.0
+    goal_message.goal.target_pose.pose.orientation.z=0.0
+    goal_message.goal.target_pose.pose.orientation.w=1.0
 
-
-while not rospy.is_shutdown():
+    rospy.loginfo("Publish a navi goal")    
+    pub.publish(goal_message)
+    '''
+    while not rospy.is_shutdown():
         rospy.loginfo("Publish a navi goal")
-        sg_message=SceneGraph()
-        sg_message.header = std_msgs.msg.Header()
-        sg_message.header.stamp = rospy.Time.now()
- 
-        sg_message.object_distance = 5.0
-        sg_message.object_direction = 0
-        sg_message.object_speed  = 0.0
-        sg_message.object_orientation = 0
-        pub.publish(sg_message)
+        pub.publish(goal_message)
         rate.sleep()
-
+    '''
 
 if __name__ == '__main__':
     try:
