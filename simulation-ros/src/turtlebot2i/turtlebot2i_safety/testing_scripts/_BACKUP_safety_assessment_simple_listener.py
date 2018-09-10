ed80ffe0be4ea727941953aae68b4ed558b2ffc1
Enyu Cao <caoe@kth.se>
2018-09-10 10:33:32 +0200

    Try to update FLS config.

--- simulation-ros/src/turtlebot2i/turtlebot2i_safety/testing_scripts/_BACKUP_safety_assessment_simple_listener.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_safety/testing_scripts/_BACKUP_safety_assessment_simple_listener.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from turtlebot2i_safety.msg import SceneGraph 
+from turtlebot2i_safety.msg import SceneGraphSingle 
 #import SceneGraph 
 import std_msgs.msg
 
@@ -9,10 +9,10 @@ def sg_cb(data):
     rospy.loginfo("I got %f at %s",data.object_distance,data.header.stamp)
 
 def listener(): 
-    pub = rospy.Publisher('topic_name', SceneGraph, queue_size=10)
+    
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("/turtlebot2i_safety/SceneGraph", SceneGraph, sg_cb)
+    rospy.Subscriber("/turtlebot2i_safety/SceneGraph", SceneGraphSingle, sg_cb)
     
     rospy.spin()
 
--- simulation-ros/src/turtlebot2i/turtlebot2i_safety/testing_scripts/safety_assessment_simple_talker.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_safety/testing_scripts/safety_assessment_simple_talker.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 import rospy
-from turtlebot2i_safety.msg import SceneGraph 
+from turtlebot2i_safety.msg import SceneGraphSingle 
 #import SceneGraph 
 import std_msgs.msg
 
 def talker(): 
-    pub = rospy.Publisher('/turtlebot2i_safety/SceneGraph', SceneGraph, queue_size=10)
+    pub = rospy.Publisher('/turtlebot2i_safety/SceneGraph', SceneGraphSingle, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10 Hz
     while not rospy.is_shutdown():
         rospy.loginfo("Publish a topic")
-        sg_message=SceneGraph()
+        sg_message=SceneGraphSingle()
         sg_message.header = std_msgs.msg.Header()
         sg_message.header.stamp = rospy.Time.now()
  
-        sg_message.object_distance = 5.0
+        sg_message.object_distance = 1.00
         sg_message.object_direction = 0
         sg_message.object_speed  = 0.0
         sg_message.object_orientation = 0
