27ae8ece11c3d5fc2c9c176ca249ed8b53e365d3
Enyu Cao <caoe@kth.se>
2018-09-19 13:18:55 +0200

    FLS improved, but need more rules

--- simulation-ros/src/turtlebot2i/turtlebot2i_safety/developing_scripts/rules.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_safety/developing_scripts/rules.py
@@ -1,178 +1,143 @@
 #!/usr/bin/env python
 """
-==========================================
-Fuzzy Control Systems: The Tipping Problem
-==========================================
+ Fuzzy logic system for risk assessment
 
-The 'tipping problem' is commonly used to illustrate the power of fuzzy logic
-principles to generate complex behavior from a compact, intuitive set of
-expert rules.
-
-If you're new to the world of fuzzy control systems, you might want
-to check out the `Fuzzy Control Primer
-<../userguide/fuzzy_control_primer.html>`_
-before reading through this worked example.
-
-The Tipping Problem
--------------------
-
-Let's create a fuzzy control system which models how you might choose to tip
-at a restaurant.  When tipping, you consider the service and food quality,
-rated between 0 and 10.  You use this to leave a tip of between 0 and 25%.
-
-We would formulate this problem as:
-
-* Antecednets (Inputs)
-   - `service`
-      * Universe (ie, crisp value range): How good was the service of the wait
-        staff, on a scale of 0 to 10?
-      * Fuzzy set (ie, fuzzy value range): poor, acceptable, amazing
-   - `food quality`
-      * Universe: How tasty was the food, on a scale of 0 to 10?
-      * Fuzzy set: bad, decent, great
-* Consequents (Outputs)
-   - `tip`
-      * Universe: How much should we tip, on a scale of 0% to 25%
-      * Fuzzy set: low, medium, high
-* Rules
-   - IF the *service* was good  *or* the *food quality* was good,
-     THEN the tip will be high.
-   - IF the *service* was average, THEN the tip will be medium.
-   - IF the *service* was poor *and* the *food quality* was poor
-     THEN the tip will be low.
-* Usage
-   - If I tell this controller that I rated:
-      * the service as 9.8, and
-      * the quality as 6.5,
-   - it would recommend I leave:
-      * a 20.2% tip.
-
-
-Creating the Tipping Controller Using the skfuzzy control API
--------------------------------------------------------------
-
-We can use the `skfuzzy` control system API to model this.  First, let's
-define fuzzy variables
+    Input: Scene-graph content
+    Output: Risk level
 """
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-# New Antecedent/Consequent objects hold universe variables and membership
-# functions
-quality = ctrl.Antecedent(np.arange(0, 11, 1), 'quality')   # We can have a larger input: No range check needed.
-service = ctrl.Antecedent(np.arange(0, 11, 1), 'service')   # 
-tip = ctrl.Consequent(np.arange(0, 26, 1), 'tip')
+global IZW # Safety zone size
+IZW = 0.4  # This is an example. It will be received as a ROS topic.
 
+# Antecedent/Consequent objects hold universe variables and membership functions
+# Universe variables: Here we define the Antecedent/Consequent range 
+# We can have a input outside the range: No range check
+# np.arange(min, max, step) : step will influence accuracy (esp. curve e.g. Gaussian MF)
+step_meter = 0.02 # If the step are large, the Gaussian MF will regress to Triangular MF
+step_meter_per_second = 0.02
+step_risk = 0.05
+range_degree = np.arange(-180, 180+1, 1.0)    # Range: -180 degree ~ 180 degree for direction and orientation
+range_meter  = np.arange(0, 3.0+step_meter, step_meter)         # Range:  0 meter ~ 3 meter for distance
+range_meter_per_second = np.arange(0, 2.0+step_meter_per_second, step_meter_per_second)#Range:  0 mps ~ 2 mps for speed
+range_risk = np.arange(0, 5+step_risk, step_risk)  # Range: 0,1,2,3,4 for risk
+
+object_distance = ctrl.Antecedent(range_meter, 'distance')     # 0- 3  meter
+object_direction  = ctrl.Antecedent(range_degree , 'direction') # -180~180 degree
+object_speed = ctrl.Antecedent(range_meter_per_second , 'speed')		#0- 3 m/s
+object_orientation = ctrl.Antecedent(range_degree , 'orientation')#-180~180 degree
+
+object_risk = ctrl.Consequent(range_risk, 'risk')
+
+
+# Custom membership functions can be built interactively with a familiar Pythonic API
+distance_p1 = fuzz.gaussmf(range_meter,IZW,0.1)
+distance_p2 = fuzz.gaussmf(range_meter,IZW,0.1) 
+# Distance
+object_distance['Near']  = fuzz.gaussmf(range_meter,0.5*IZW,0.1) # skfuzzy.membership.gaussmf(x, mean, sigma)
+object_distance['Medium']= fuzz.gaussmf(range_meter,IZW,0.1)     # x: range_meter or object_distance.universe
+object_distance['Far']   = fuzz.gaussmf(range_meter,2.0*IZW,0.1)
+#object_distance.view()
+
+# Speed 
+object_speed['Slow']  = fuzz.gaussmf(range_meter_per_second,0.5,0.2)
+object_speed['Medium']= fuzz.gaussmf(range_meter_per_second,1.0,0.2)
+object_speed['Fast']  = fuzz.gaussmf(range_meter_per_second,1.5,0.2)
+#object_speed.view()
+
+# Direction
+object_direction['Front']  = fuzz.gaussmf(range_degree,0,15)
+object_direction['FrontLeft']= fuzz.gaussmf(range_degree,45,15)
+object_direction['Left']= fuzz.gaussmf(range_degree,90,15)
+object_direction['FrontRight']  = fuzz.gaussmf(range_degree,-45,15)
+object_direction['Right']  = fuzz.gaussmf(range_degree,-90,15)
+rear_d_p1 = fuzz.gaussmf(range_degree,180,60)
+rear_d_p2 = fuzz.gaussmf(range_degree,-180,60) 
+null,object_direction['BigRear']  =fuzz.fuzzy_or(range_degree,rear_d_p1,range_degree,rear_d_p2)
+#object_direction.view()
+
+# Orientation
+object_orientation['Front']  = fuzz.gaussmf(range_degree,0,15)
+object_orientation['FrontLeft']= fuzz.gaussmf(range_degree,45,15)
+object_orientation['Left']= fuzz.gaussmf(range_degree,90,15)
+object_orientation['RearLeft']= fuzz.gaussmf(range_degree,135,15)
+rear_p1 = fuzz.gaussmf(range_degree,180,15)
+rear_p2 = fuzz.gaussmf(range_degree,-180,15) 
+null,object_orientation['Rear']  =fuzz.fuzzy_or(range_degree,rear_p1,range_degree,rear_p2)
+object_orientation['RearRight']  = fuzz.gaussmf(range_degree,-135,15)
+object_orientation['Right']  = fuzz.gaussmf(range_degree,-90,15)
+object_orientation['FrontRight']  = fuzz.gaussmf(range_degree,-45,15) 
+object_orientation.view()
+
+object_risk['VeryLow'] = fuzz.gaussmf(range_risk,0,0.3)
+object_risk['Low'] = fuzz.gaussmf(range_risk,1,0.3)
+object_risk['Medium'] = fuzz.gaussmf(range_risk,2,0.3)
+object_risk['High'] = fuzz.gaussmf(range_risk,3,0.3)
+object_risk['VeryHigh'] = fuzz.gaussmf(range_risk,4,0.3)
 # Auto-membership function population is possible with .automf(3, 5, or 7)
-quality.automf(3)
-service.automf(3)
+#object_risk.automf(5) #tri mf
+#object_risk.view()
 
-# Custom membership functions can be built interactively with a familiar,
-# Pythonic API
-x = np.r_[0:26:1]
-tip['low'] = fuzz.trimf(tip.universe, [0, 0, 13])
-tip['medium1'] =fuzz.trimf(tip.universe, [0, 13, 15])#fuzz.gaussmf(tip.universe,13,1)
-tip['medium2'] =fuzz.trimf(tip.universe, [15, 19, 25])#fuzz.gaussmf(tip.universe,3,1)
-null,tip['medium'] = fuzz.fuzzy_or(x,tip['medium1'],x,tip['medium2']) 
-#fuzz.gauss2mf(tip.universe,3,1,20,1)    #fuzz.gaussmf(tip.universe,13,3)#fuzz.trimf(tip.universe, [0, 13, 25])
-tip['high'] = fuzz.trimf(tip.universe, [13, 25, 25])
-
+#raw_input() # Debug
 """
-To help understand what the membership looks like, use the ``view`` methods.
-"""
-
-# You can see how these look with .view()
-quality['average'].view()
-"""
-.. image:: PLOT2RST.current_figure
-"""
-service.view()
-"""
-.. image:: PLOT2RST.current_figure
-"""
-tip.view()
-"""
-.. image:: PLOT2RST.current_figure
-
-
 Fuzzy rules
 -----------
-
-Now, to make these triangles useful, we define the *fuzzy relationship*
-between input and output variables. For the purposes of our example, consider
-three simple rules:
-
-1. If the food is poor OR the service is poor, then the tip will be low
-2. If the service is average, then the tip will be medium
-3. If the food is good OR the service is good, then the tip will be high.
-
-Most people would agree on these rules, but the rules are fuzzy. Mapping the
-imprecise rules into a defined, actionable tip is a challenge. This is the
-kind of task at which fuzzy logic excels.
+    Straight forward rules. 
 """
+rule1 =  ctrl.Rule(object_direction['BigRear'],object_risk['VeryLow']) # We ignore objects behind it.
+rule2 =  ctrl.Rule(object_distance['Near'], object_risk['VeryHigh']  ) # Too near object is dangerous.
+rule3 =  ctrl.Rule(object_distance['Far'], object_risk['VeryLow'] )    # Too far object is safe.
 
-rule1 = ctrl.Rule(quality['poor'] | service['poor'], tip['low'])
-rule2 = ctrl.Rule(service['average'], tip['medium'])
-rule3 = ctrl.Rule(service['good'] | quality['good'], tip['high'])
+rule3 =  ctrl.Rule( object_speed['Medium'] & object_direction['Front'] & object_speed['Slow']  , object_risk['High']  )
+rule4 =  ctrl.Rule( object_speed['Medium'] & object_direction['FrontLeft'] & object_speed['Slow']  , object_risk['Medium']  )
+rule5 =  ctrl.Rule( object_speed['Medium'] & object_direction['Left'] & object_speed['Slow']  , object_risk['Low']  )
+rule6 =  ctrl.Rule( object_speed['Medium'] & object_direction['FrontRight'] & object_speed['Slow']  , object_risk['Medium']  )
+rule7 =  ctrl.Rule( object_speed['Medium'] & object_direction['Right'] & object_speed['Slow'] & object_orientation['Front'] , object_risk['Low']  )
+
+
+#rule =  ctrl.Rule( object_speed[' '] & object_direction[' '] & object_speed[' ']  , object_risk[' ']  )
+
 
 rule1.view()
 
 """
-.. image:: PLOT2RST.current_figure
 
 Control System Creation and Simulation
 ---------------------------------------
 
-Now that we have our rules defined, we can simply create a control system
-via:
+Now that we have our rules defined, we can simply create a control system via:
 """
 
-tipping_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
+ra_fls = ctrl.ControlSystem([rule1, rule2, rule3,rule4, rule5, rule6, rule7])
 
 """
 In order to simulate this control system, we will create a
 ``ControlSystemSimulation``.  Think of this object representing our controller
-applied to a specific set of cirucmstances.  For tipping, this might be tipping
-Sharon at the local brew-pub.  We would create another
-``ControlSystemSimulation`` when we're trying to apply our ``tipping_ctrl``
-for Travis at the cafe because the inputs would be different.
+applied to a specific set of cirucmstances. 
 """
 
-tipping = ctrl.ControlSystemSimulation(tipping_ctrl)
+risk_assessment_instance = ctrl.ControlSystemSimulation(ra_fls)
 
 """
 We can now simulate our control system by simply specifying the inputs
-and calling the ``compute`` method.  Suppose we rated the quality 6.5 out of 10
-and the service 9.8 of 10.
+and calling the ``compute`` method.  
 """
 # Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
 # Note: if you like passing many inputs all at once, use .inputs(dict_of_data)
-tipping.input['quality'] = 10.0
-tipping.input['service'] = 10.0
+risk_assessment_instance.input['distance'] = 0.5		
+risk_assessment_instance.input['direction'] = 15.3		
+risk_assessment_instance.input['speed'] =   0.2	
+risk_assessment_instance.input['orientation'] = 80.0
 
 # Crunch the numbers
-tipping.compute()
+risk_assessment_instance.compute()
 
 """
 Once computed, we can view the result as well as visualize it.
 """
-print tipping.output['tip']
-tip.view(sim=tipping)
+print risk_assessment_instance.output['risk']
+object_risk.view(sim=risk_assessment_instance)
 raw_input()
-"""
-.. image:: PLOT2RST.current_figure
 
-The resulting suggested tip is **20.24%**.
-
-Final thoughts
---------------
-
-The power of fuzzy systems is allowing complicated, intuitive behavior based
-on a sparse system of rules with minimal overhead. Note our membership
-function universes were coarse, only defined at the integers, but
-``fuzz.interp_membership`` allowed the effective resolution to increase on
-demand. This system can respond to arbitrarily small changes in inputs,
-and the processing burden is minimal.
-
-"""
