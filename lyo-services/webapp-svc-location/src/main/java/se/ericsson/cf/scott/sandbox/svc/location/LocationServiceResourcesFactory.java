e776570414bee4d39b049bfa0897ef4d2144eac1
Andrew Berezovskyi <andriib@kth.se>
2019-01-16 09:48:20 +0100

    Initial commit for the Location Service
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ lyo-services/webapp-svc-location/src/main/java/se/ericsson/cf/scott/sandbox/svc/location/LocationServiceResourcesFactory.java
@@ -0,0 +1,45 @@
+// Start of user code Copyright
+/*******************************************************************************
+ * Copyright (c) 2017 Jad El-khoury.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ *
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *
+ *     Jad El-khoury        - initial implementation
+ *     
+ *******************************************************************************/
+// End of user code
+
+package se.ericsson.cf.scott.sandbox.svc.location;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.UriBuilder;
+
+import org.eclipse.lyo.oslc4j.core.model.Link;
+import org.eclipse.lyo.oslc4j.core.OSLC4JUtils;
+
+// Start of user code imports
+// End of user code
+
+// Start of user code pre_class_code
+// End of user code
+
+public class LocationServiceResourcesFactory {
+
+    // Start of user code class_attributes
+    // End of user code
+    
+    // Start of user code class_methods
+    // End of user code
+
+}
