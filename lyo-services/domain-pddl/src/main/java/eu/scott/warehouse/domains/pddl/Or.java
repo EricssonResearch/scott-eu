cc00cf938917e99e793d5d488470c336915d2798
Andrew Berezovskyi <andriib@kth.se>
2019-01-30 00:04:45 +0100

    Regenerate DS classes
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- lyo-services/domain-pddl/src/main/java/eu/scott/warehouse/domains/pddl/Or.java
+++ lyo-services/domain-pddl/src/main/java/eu/scott/warehouse/domains/pddl/Or.java
@@ -30,29 +30,53 @@ package eu.scott.warehouse.domains.pddl;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.text.SimpleDateFormat;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import javax.ws.rs.core.UriBuilder;
+
 import org.eclipse.lyo.oslc4j.core.OSLC4JUtils;
+import org.eclipse.lyo.oslc4j.core.exception.OslcCoreApplicationException;
+import org.eclipse.lyo.oslc4j.core.annotation.OslcAllowedValue;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcDescription;
+import org.eclipse.lyo.oslc4j.core.annotation.OslcMemberProperty;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcName;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcNamespace;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcOccurs;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcPropertyDefinition;
+import org.eclipse.lyo.oslc4j.core.annotation.OslcRange;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcReadOnly;
+import org.eclipse.lyo.oslc4j.core.annotation.OslcRepresentation;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcResourceShape;
+import org.eclipse.lyo.oslc4j.core.annotation.OslcTitle;
 import org.eclipse.lyo.oslc4j.core.annotation.OslcValueType;
-import org.eclipse.lyo.oslc4j.core.exception.OslcCoreApplicationException;
 import org.eclipse.lyo.oslc4j.core.model.AbstractResource;
 import org.eclipse.lyo.oslc4j.core.model.Link;
 import org.eclipse.lyo.oslc4j.core.model.Occurs;
 import org.eclipse.lyo.oslc4j.core.model.OslcConstants;
+import org.eclipse.lyo.oslc4j.core.model.Representation;
+import org.eclipse.lyo.oslc4j.core.model.ValueType;
 import org.eclipse.lyo.oslc4j.core.model.ResourceShape;
 import org.eclipse.lyo.oslc4j.core.model.ResourceShapeFactory;
-import org.eclipse.lyo.oslc4j.core.model.ValueType;
+
+import eu.scott.warehouse.domains.pddl.PddlDomainConstants;
+
+
+import eu.scott.warehouse.domains.pddl.PddlDomainConstants;
 
 // Start of user code imports
+import org.checkerframework.checker.nullness.qual.NonNull;
 // End of user code
 
 // Start of user code preClassCode
