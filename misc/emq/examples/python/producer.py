69d64d9327bf454889c3a5341cbf18214caf892e
Andrew Berezovskyi <andriib@kth.se>
2019-07-11 20:12:40 +0200

    Erlang MQTT Broker (#178)
    * simple broker and producer/consumer example in python
    * Move the files under /misc
    * Rename mqtt to emq
    * Build misc (incl. optic planner)
    * Update to EMQ 2.3.9 and Ubuntu 18.04
    * Switch to lyo.core@master

---
+++ misc/emq/examples/python/producer.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+import time
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    #client.subscribe("meeting_room1")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+while True:
+	client.publish("meeting_room1", payload="23")
+	time.sleep(5)
+	client.loop()
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+#client.loop_forever()
