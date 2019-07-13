903f8846cffa87aabc9925925f1d0dad2adec5a9
ahmadterra <ahmad.terra@ericsson.com>
2019-07-13 02:15:30 +0200

    CNN implementation

--- simulation-ros/src/turtlebot2i/turtlebot2i_safety/src/training_rl.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_safety/src/training_rl.py
@@ -15,9 +15,9 @@ from rl_environment import Env
 import tensorflow as tf
 from keras.models import Sequential, load_model
 from keras.optimizers import RMSprop
-from keras.layers import Dense, Dropout, Activation
+from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D
+from keras.layers import Dense, Dropout, Activation, Flatten
 from turtlebot2i_safety.msg import SafetyRisk
-
 
 
 class ReinforceAgent():
@@ -30,11 +30,14 @@ class ReinforceAgent():
         self.load_model = False
         self.load_episode = 0#340 #0
         self.env = env
+        self.stack_len = 20
+        self.state_stack = deque(maxlen=self.stack_len)
         if training_mode:
-            self.state = self.env.reset()
+            self.single_state_len = len(self.env.reset())
         else:
-            self.state = self.env.getEmptyState()
-        self.state_size = len(self.state)
+            self.single_state_len = len(self.env.getEmptyState())
+        self.state_size = (self.stack_len, self.single_state_len)
+        self.state = np.empty((self.state_size), dtype=np.float64)
         self.action_size = self.env.action_size
         self.episode_step = 6000
         self.target_update = 2000
@@ -91,10 +94,12 @@ class ReinforceAgent():
         model = Sequential()
         dropout = 0.2
 
-        model.add(Dense(64, input_shape=(self.state_size,), activation='relu', kernel_initializer='lecun_uniform'))
-
-        model.add(Dense(48, activation='relu', kernel_initializer='lecun_uniform'))
-        model.add(Dense(32, activation='relu', kernel_initializer='lecun_uniform'))
+        model.add(Conv1D(32, 3, activation='relu', input_shape=self.state_size))
+        model.add(Conv1D(32, 3, activation='relu'))
+        #model.add(MaxPooling1D(3))
+        model.add(Conv1D(32, 3, activation='relu'))
+        model.add(GlobalAveragePooling1D())
+        #model.add(Dense(64, activation='relu', kernel_initializer='lecun_uniform'))
         model.add(Dropout(dropout))
 
         model.add(Dense(self.action_size, kernel_initializer='lecun_uniform'))
@@ -120,7 +125,7 @@ class ReinforceAgent():
             return random.randrange(self.action_size)
         else:
             with self.graph.as_default():
-                q_value = self.model.predict(state.reshape(1, len(state)))
+                q_value = self.model.predict(state)
             self.q_value = q_value
             return np.argmax(q_value[0])
 
@@ -129,7 +134,7 @@ class ReinforceAgent():
 
     def trainModel(self, target=False):
         mini_batch = random.sample(self.memory, self.batch_size)
-        X_batch = np.empty((0, self.state_size), dtype=np.float64)
+        X_batch = np.empty((0, self.stack_len, self.single_state_len), dtype=np.float64)
         Y_batch = np.empty((0, self.action_size), dtype=np.float64)
 
         with self.graph.as_default():
@@ -139,84 +144,99 @@ class ReinforceAgent():
                 rewards = mini_batch[i][2]
                 next_states = mini_batch[i][3]
                 dones = mini_batch[i][4]
-
-                q_value = self.model.predict(states.reshape(1, len(states)))
+                #print("states shape",np.shape(states),"|xbatch shape:",np.shape(X_batch))
+                q_value = self.model.predict(states)
                 self.q_value = q_value
 
                 if target:
-                    next_target = self.target_model.predict(next_states.reshape(1, len(next_states)))
+                    next_target = self.target_model.predict(next_states)
 
                 else:
-                    next_target = self.model.predict(next_states.reshape(1, len(next_states)))
+                    next_target = self.model.predict(next_states)
 
                 next_q_value = self.getQvalue(rewards, next_target, dones)
 
-                X_batch = np.append(X_batch, np.array([states.copy()]), axis=0)
+                X_batch = np.append(X_batch, np.array(states.copy()), axis=0)
                 Y_sample = q_value.copy()
 
                 Y_sample[0][actions] = next_q_value
                 Y_batch = np.append(Y_batch, np.array([Y_sample[0]]), axis=0)
 
                 if dones:
-                    X_batch = np.append(X_batch, np.array([next_states.copy()]), axis=0)
+                    X_batch = np.append(X_batch, np.array(next_states.copy()), axis=0)
                     Y_batch = np.append(Y_batch, np.array([[rewards] * self.action_size]), axis=0)
 
             self.model.fit(X_batch, Y_batch, batch_size=self.batch_size, epochs=1, verbose=0)    
 
     def training_callback(self, data):
-        next_state, done = self.env.getState(data)
-        reward = self.env.setReward(next_state, done, self.action)
-        next_state = np.asarray(next_state)
-
-        self.appendMemory(self.state, self.action, reward, next_state, done)
-
-        self.state = next_state
-        self.action = agent.getAction(self.state)
-        self.env.execute(self.action)
-        self.score += reward
-        if len(self.memory) >= self.train_start:
-            if self.global_step <= self.target_update:
-                self.trainModel()
-            else:
-                self.trainModel(True)
-
-        if self.t >= 2000:
-            rospy.loginfo("Time out!!")
-            done = True
-
-        if done:
-            if (self.e % 5 == 0) or (self.score > -4000):
-                self.model.save(self.dirPath + str(self.e) + '.h5')
-                with open(self.dirPath + str(self.e) + '.json', 'w') as outfile:
-                    json.dump(self.param_dictionary, outfile)
-                np.savez(self.dirPath + str(self.e) + '.npz', scores=self.scores, episodes=self.episodes, maxQval=self.maxQval, step_list=self.step_list)
-
-            self.state = self.env.reset(data=data)
-            self.updateTargetModel()
-            self.scores.append(self.score)
-            self.episodes.append(self.e)
-            self.maxQval.append(np.max(self.q_value))
-            self.step_list.append(self.global_step)
-            m, s = divmod(int(time.time() - self.start_time), 60)
-            h, m = divmod(m, 60)
-
-            if self.e>2:
-                rospy.loginfo('Ep: %d score: %.2f average_score: %.2f memory: %d epsilon: %.2f time: %d:%02d:%02d',
-                              self.e, self.score, self.score/(self.step_list[-1]-self.step_list[-2]), len(self.memory), self.epsilon, h, m, s)
-            else:
-                rospy.loginfo('Ep: %d score: %.2f average_score: %.2f memory: %d epsilon: %.2f time: %d:%02d:%02d',
-                              self.e, self.score, self.score/self.step_list[-1], len(self.memory), self.epsilon, h, m, s)
-            self.param_dictionary = dict(zip(['epsilon'], [self.epsilon]))
-            #break
-
-            self.score = 0
-            self.e += 1
-            self.t = 0
-            if agent.epsilon > agent.epsilon_min:
-                agent.epsilon *= agent.epsilon_decay
+        if len(self.state_stack) < self.stack_len:
+            state, done = self.env.getState(data)
+            self.state_stack.append(np.asarray(state))
+            self.env.execute(7) #scaling to 0.4 for both wheels
+            if len(self.state_stack) == self.stack_len:
+                self.state = np.asarray([self.state_stack])
+            if done:
+                self.state_stack.clear()
+                self.env.reset(data=data)
+                
         else:
-            self.t += 1
-            self.global_step += 1
+            #print("states shape:",np.shape(self.state))
+            next_state, done = self.env.getState(data)
+            reward = self.env.setReward(next_state, done, self.action)
+            #next_state = np.asarray(next_state)
+            self.state_stack.append(np.asarray(next_state))
+            next_state_stack = np.asarray([self.state_stack])
+            self.appendMemory(self.state, self.action, reward, next_state_stack, done)
+
+            self.state = next_state_stack
+            self.action = agent.getAction(self.state)
+            self.env.execute(self.action)
+            self.score += reward
+            if len(self.memory) >= self.train_start:
+                if self.global_step <= self.target_update:
+                    self.trainModel()
+                else:
+                    self.trainModel(True)
+
+            if self.t >= 2000:
+                rospy.loginfo("Time out!!")
+                done = True
+
+            if done:
+                if (self.e % 5 == 0) or (self.score > -4000):
+                    self.model.save(self.dirPath + str(self.e) + '.h5')
+                    with open(self.dirPath + str(self.e) + '.json', 'w') as outfile:
+                        json.dump(self.param_dictionary, outfile)
+                    np.savez(self.dirPath + str(self.e) + '.npz', scores=self.scores, episodes=self.episodes, maxQval=self.maxQval, step_list=self.step_list)
+
+                #self.state = self.env.reset(data=data)
+                self.env.reset(data=data)
+                self.updateTargetModel()
+                self.scores.append(self.score)
+                self.episodes.append(self.e)
+                self.maxQval.append(np.max(self.q_value))
+                self.step_list.append(self.global_step)
+                m, s = divmod(int(time.time() - self.start_time), 60)
+                h, m = divmod(m, 60)
+
+                if self.e>2:
+                    rospy.loginfo('Ep: %d score: %.2f average_score: %.2f memory: %d epsilon: %.2f time: %d:%02d:%02d',
+                                  self.e, self.score, self.score/(self.step_list[-1]-self.step_list[-2]), len(self.memory), self.epsilon, h, m, s)
+                else:
+                    rospy.loginfo('Ep: %d score: %.2f average_score: %.2f memory: %d epsilon: %.2f time: %d:%02d:%02d',
+                                  self.e, self.score, self.score/self.step_list[-1], len(self.memory), self.epsilon, h, m, s)
+                self.param_dictionary = dict(zip(['epsilon'], [self.epsilon]))
+                #break
+
+                self.score = 0
+                self.e += 1
+                self.t = 0
+                self.state_stack.clear()
+                if agent.epsilon > agent.epsilon_min:
+                    agent.epsilon *= agent.epsilon_decay
+            else:
+                self.t += 1
+                self.global_step += 1
 
     def prediction_callback(self, data):
         next_state, done = self.env.getState(data)
