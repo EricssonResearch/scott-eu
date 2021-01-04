96665216eaba449160a1a29f0bc34f379a3fc0f5
Franco Ruggeri <fruggeri@kth.se>
2021-01-04 13:34:42 +0100

    add proxy service for scene graph generation
    The proxy service provides more information than the plain service (e.g. latency and execution time) and so is useful to train the offloading policy

---
+++ simulation-ros/src/turtlebot2i/turtlebot2i_edge/scripts/scene_graph_proxy.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import rospy
+from turtlebot2i_scene_graph.srv import GenerateSceneGraph as GenerateSceneGraphOut
+from turtlebot2i_edge.srv import GenerateSceneGraph as GenerateSceneGraphIn
+from turtlebot2i_edge.srv import GenerateSceneGraphResponse
+
+
+def proxy(generate_scene_graph, request):
+    time_start = rospy.Time.now()
+    response = generate_scene_graph()
+    time_end = rospy.Time.now()
+
+    communication_latency = time_start - request.header.stamp
+    execution_latency = time_end - time_start
+
+    return GenerateSceneGraphResponse(
+        communication_latency=communication_latency,
+        execution_latency=execution_latency,
+        scene_graph=response.scene_graph
+    )
+
+
+def main():
+    rospy.init_node('scene_graph_proxy')
+
+    rospy.loginfo('Waiting for ROS service to generate scene graph...')
+    rospy.wait_for_service('generate_scene_graph')
+    generate_scene_graph = rospy.ServiceProxy('generate_scene_graph', GenerateSceneGraphOut)
+
+    rospy.Service(
+        name='generate_scene_graph_proxy',
+        service_class=GenerateSceneGraphIn,
+        handler=lambda request: proxy(generate_scene_graph, request),
+        buff_size=2**20     # 1 MB, enough for camera images
+    )
+    rospy.loginfo('ROS service to generate scene graph with proxy ready')
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
