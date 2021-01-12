462a21b5846fd26e70900f3262351854f73a86d4
Franco Ruggeri <fruggeri@kth.se>
2021-01-12 03:19:01 +0100

    update scene
    - Map for training
    - Laptop

--- simulation-ros/src/turtlebot2i/turtlebot2i_description/v-rep_model/warehouse_scene/vrep_scripts/Scene_Builder_edge.lua
+++ simulation-ros/src/turtlebot2i/turtlebot2i_description/v-rep_model/warehouse_scene/vrep_scripts/Scene_Builder_edge.lua
@@ -16,58 +16,72 @@ function sysCall_init()
     simRemoteApi.start(20001)
     simRemoteApi.start(20002)
 
-
     -- Floor
-    addModel('Floor10x15m', {2,0,0}, {0,0,0})
+    addModel('Floor15x20m', {0,0,0}, {0,0,0})
 
     -- Walls
-    addModel('80cmHighWall1000cm', {9.5,0,.4}, {0,0,0})
-    addModel('80cmHighWall1000cm', {-5.5,0,.4}, {0,0,0})
-    addModel('80cmHighWall1500cm', {2.0,5,.4}, {0,0,math.pi/2})
-    addModel('80cmHighWall1500cm', {2.0,-5,.4}, {0,0,math.pi/2})
-    addModel('80cmHighWall100cm', {-3.0,4.5,.4}, {0,0,0})
-    addModel('80cmHighWall750cm', {-3.0,-1.25,.4}, {0,0,0})
-    addModel('80cmHighWall750cm', {0.75,0.00,.4}, {0,0,math.pi/2})
-    addModel('80cmHighWall100cm', {6.0,3.5,.4}, {0,0,0})
-    addModel('80cmHighWall100cm', {4.5,4.5,.4}, {0,0,0})
-    addModel('80cmHighWall500cm', {4.5,0.5,.4}, {0,0,0})
-    addModel('80cmHighWall200cm', {4.5,-4.0,.4}, {0,0,0})
-    addModel('80cmHighWall200cm', {8.5,3.0,.4}, {0,0,math.pi/2})
-    addModel('80cmHighWall200cm', {5.5,3.0,.4}, {0,0,math.pi/2})
-    addModel('80cmHighWall200cm', {8.5,-2.0,.4}, {0,0,math.pi/2})
-    addModel('80cmHighWall200cm', {5.5,-2.0,.4}, {0,0,math.pi/2})
+    addModel('80cmHighWall1500cm', {10.0,0,.4}, {0,0,0})
+    addModel('80cmHighWall1500cm', {-10.0,0,.4}, {0,0,0})
+    addModel('80cmHighWall2000cm', {0.0,7.5,.4}, {0,0,math.pi/2})
+    addModel('80cmHighWall2000cm', {0.0,-7.5,.4}, {0,0,math.pi/2})
+    addModel('80cmHighWall500cm', {4.0,4.0,.4}, {0,0,0})
+    addModel('80cmHighWall500cm', {6.5,1.5,.4}, {0,0,math.pi/2})
+    addModel('80cmHighWall500cm', {9.0,3.0,.4}, {0,0,0})
+    addModel('80cmHighWall500cm', {5.5,6.5,.4}, {0,0,math.pi/2})
+    addModel('80cmHighWall1000cm', {-3.0,2.0,.4}, {0,0,math.pi/2})
+    addModel('80cmHighWall1500cm', {-0.5,-1.5,.4}, {0,0,math.pi/2})
+    addModel('80cmHighWall500cm', {-5.5,-4.0,.4}, {0,0,0})
+    addModel('80cmHighWall500cm', {4.5,-4.0,.4}, {0,0,0})
+    addModel('80cmHighWall500cm', {-0.5,-5.0,.4}, {0,0,0})
 
     -- Pillars
-    addModel('80cmHighPillar100cm', {-0.5,3.0,0.25}, {0,0,0})
-    addModel('80cmHighPillar100cm', {-0.5,-2.5,0.25}, {0,0,0})
-
+    addModel('80cmHighPillar100cm', {2.0,-4.0,0.35}, {0,0,0})
+    addModel('80cmHighPillar100cm', {2.0,-6.5,0.35}, {0,0,0})
+    
     -- Warehouse objects
-    addModel('Shelf_simple', {-4.0,-4.0,0.063}, {0,0,math.pi/2})
-    addModel('ConveyorBelt_simple', {2.5,-2.50,0.113}, {0,0,-math.pi/2})
-    addModel('ConcreteBox2', {-3.5,2.0,0.5}, {0,0,0})
-    addModel('ConcreteBox2', {-5.0,-0.5,0.5}, {0,0,0})
-    addModel('ConcreteBox2', {-3.5,-2.5,0.5}, {0,0,0})
-    addModel('ConcreteBox', {2.5,3.5,0.5}, {0,0,0})
-    addModel('ConcreteBox1', {7.0,1.5,0.5}, {0,0,math.pi/2})
-    addModel('ConcreteBox2', {6.0,-0.5,0.5}, {0,0,0})
-    addModel('ConcreteBox2', {8.0,-0.5,0.5}, {0,0,0})
-    addModel('ConcreteBox1', {1.0,-1.5,0.5}, {0,0,0})
-    addModel('ConcreteBox1', {1.0,-3.5,0.5}, {0,0,0})
+    addModel('ConveyorBelt_simple', {1.0,1.0,0.113}, {0,0,math.pi/2})
+    addModel('ConveyorBelt_simple', {-1.0,-0.5,0.113}, {0,0,-math.pi/2})
+    addModel('ConveyorBelt_simple', {-3.0,1.0,0.113}, {0,0,math.pi/2})
+    addModel('ConveyorBelt_simple', {-5.0,-0.5,0.113}, {0,0,-math.pi/2})
+    addModel('ConveyorBelt_simple', {-7.0,1.0,0.113}, {0,0,math.pi/2})
+    addModel('ConveyorBelt_simple', {-4.5,-6.0,0.113}, {0,0,math.pi})
+    addModel('ConveyorBelt_simple', {0.0,4.5,0.113}, {0,0,0})
+    addModel('ConveyorBelt_simple', {-9.0,5.5,0.113}, {0,0,math.pi})
+    addModel('ConveyorBelt_simple', {-9.0,-3.0,0.113}, {0,0,math.pi})
+    addModel('ConveyorBelt_simple', {-4.0,5.0,0.113}, {0,0,math.pi/2})
+    addModel('ConveyorBelt_simple', {-7.0,3.0,0.113}, {0,0,-math.pi/2})
+    addModel('ConcreteBox1', {-2.0,-3.0,0.5}, {0,0,math.pi/2})
+    addModel('ConcreteBox2', {1.0,7.0,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {-3.0,-4.5,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {5.5,0.0,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {5.5,-4.0,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {7.0,-2.5,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {7.25,-4.5,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {9.0,-4.5,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {7.5,-6.5,0.5}, {0,0,0})
+    addModel('ConcreteBox2', {-7.0,-6.0,0.5}, {0,0,0})
 
-    -- Dock stations (1 for each robot, otherwise there are errors!)
-    addModel('dockstation', {-5.25,-4.000,0.063}, {0,0,math.pi/2})
+    -- Laptop
+    addModel('laptop', {7.0,4.5,0.5}, {-1,0,-math.pi/2})
+
+    -- Dock stations (1 for each robot,otherwise there are errors!)
+    addModel('dockstation', {-6.5,-2.0,0.063}, {0,0,0})
 
     -- Robots
-    addModel('turtlebot2i', {-4.8,-4,0.063}, {0,0,math.pi/2})
+    addModel('turtlebot2i', {0.0,0.0,0.063}, {0,0,0})
 
+    -- Humans (after robots, otherwise the robots are spawn with name not starting from 0)
+--     addModel('Walking_Bill', {3.0,-2.5,0}, {0,0,-math.pi/2})
+--     addModel('Walking_Bill', {-6.5,6.5,0}, {0,0,-math.pi/2})
+--     addModel('Walking_Bill', {0.0,3.0,0}, {0,0,math.pi/2})
 end
 
-function addModel(name, position, orientation)
+function addModel(name,position,orientation)
     scenePath = sim.getStringParameter(sim.stringparam_scene_path) -- retrieve scene path
     file = '/vrep_models/'..name..'.ttm'
     objectHandle=sim.loadModel(scenePath..file)
-    result=sim.setObjectPosition(objectHandle, -1, position)
-    result=sim.setObjectOrientation(objectHandle, -1, orientation)
+    result=sim.setObjectPosition(objectHandle,-1,position)
+    result=sim.setObjectOrientation(objectHandle,-1,orientation)
     return objectHandle
 end
 
