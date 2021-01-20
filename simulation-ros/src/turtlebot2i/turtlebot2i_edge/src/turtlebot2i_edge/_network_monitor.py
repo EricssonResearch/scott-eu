4916f013a7d9013764da85b127a6cced03ab7beb
Franco Ruggeri <fruggeri@kth.se>
2021-01-20 02:28:13 +0100

    update network monitor
    - Adaptive ping period (before the period was 1 second)
    - Deadline: now it is possible to specify the time for which to monitor the network, instead of the number of packets to send
    - Handle throughput measurement when there is no connectivity

--- simulation-ros/src/turtlebot2i/turtlebot2i_edge/src/turtlebot2i_edge/_network_monitor.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_edge/src/turtlebot2i_edge/_network_monitor.py
@@ -14,16 +14,14 @@ class NetworkMonitor:
             self._stamp = rospy.ServiceProxy('edge/stamp', Stamp)
             self._stamp.wait_for_service()
 
-    def measure_latency(self, count=10):
+    def measure_latency(self, time=1):
         """Adapted from https://github.com/larics/python-network-tools/blob/master/network-test.py"""
         ping = subprocess.Popen(
-            args=['ping', self.server_host, '-c', str(count)],
+            args=['ping', self.server_host, '-A', '-w', str(time)],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
         output, _ = ping.communicate()
-
-        # TODO: prova con server irragiungibile
 
         summary = re.findall(r'rtt min/avg/max/mdev = (\S+)', output)[0]
         rtt_min, rtt_avg, rtt_max, rtt_mdev = (float(x) * 1e-3 for x in summary.split('/'))
@@ -31,19 +29,21 @@ class NetworkMonitor:
 
         return rtt_min, rtt_avg, rtt_max, rtt_mdev, packet_loss
 
-    def measure_throughput(self, size=100000):
+    def measure_throughput(self, size=2**10):
         if not self.throughput:
             raise Exception('NetworkMonitor initialized with throughput=False')
 
-        # TODO: prova con server irragiungibile (ServiceException?)
+        try:
+            self._stamp.wait_for_service(timeout=0.1)
+            bytes_ = b'\x00' * size
+            time_start = rospy.Time.now()
+            response = self._stamp(bytes=bytes_)
+            time_end = response.header.stamp
 
-        bytes_ = b'\x00' * size
-        time_start = rospy.Time.now()
-        response = self._stamp(bytes=bytes_)
-        time_end = response.header.stamp
-
-        time_elapsed = time_end - time_start
-        time_elapsed = time_elapsed.to_sec()
-        throughput = size * 8 / time_elapsed
+            time_elapsed = time_end - time_start
+            time_elapsed = time_elapsed.to_sec()
+            throughput = size * 8 / time_elapsed
+        except rospy.ROSException, rospy.ServiceException:
+            throughput = 0
 
         return throughput
