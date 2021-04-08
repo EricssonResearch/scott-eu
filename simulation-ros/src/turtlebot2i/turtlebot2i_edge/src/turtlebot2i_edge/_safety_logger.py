2c9d606d99290cc3fa1d90a93818d52142bd4d7f
Franco Ruggeri <franco.ruggeri@ericsson.com>
2021-04-08 14:56:17 +0200

    update logging for safety metrics

--- simulation-ros/src/turtlebot2i/turtlebot2i_edge/src/turtlebot2i_edge/_safety_logger.py
+++ simulation-ros/src/turtlebot2i/turtlebot2i_edge/src/turtlebot2i_edge/_safety_logger.py
@@ -28,18 +28,9 @@ class SafetyLogger(Callback):
         self._collision = False
         rospy.Subscriber('events/bumper', BumperEvent, self._update_collisions)
 
-        self._risk_lock = threading.Lock()
-        self._risk_value_sum = 0
-        self._risk_value_count = 0
-        self._risk_value_max = 0
-        self._risk_value_x_speed_sum = 0
-        self._risk_value_x_speed_max = 0
-        rospy.Subscriber('safety/risk_val', Float64, self._update_risk_values, queue_size=1)
-
         self._speed_lock = threading.Lock()
         self._speed = 0
-        self._speed_sum = 0
-        self._speed_count = 0
+        rospy.Subscriber('safety/risk_val', Float64, self._update_risk_values, queue_size=1)
         rospy.Subscriber('odom', Odometry, self._update_speed, queue_size=1)
 
         self._safety_zone_lock = threading.Lock()
@@ -51,8 +42,8 @@ class SafetyLogger(Callback):
         self._in_critical_zone = False
         self._in_warning_zone_start = None
         self._in_critical_zone_start = None
-        self._warning_zone_duration = rospy.Duration.from_sec(0)
-        self._critical_zone_duration = rospy.Duration.from_sec(0)
+        self._in_warning_zone_duration = rospy.Duration.from_sec(0)
+        self._in_critical_zone_duration = rospy.Duration.from_sec(0)
         self._mean_distance_sum = 0
         self._mean_distance_count = 0
         self._mean_min_distance = np.zeros(self.sensors)
@@ -69,17 +60,8 @@ class SafetyLogger(Callback):
             self._collisions = 0
             self._collision = False
 
-        with self._risk_lock:
-            self._risk_value_sum = 0
-            self._risk_value_count = 0
-            self._risk_value_max = 0
-            self._risk_value_x_speed_sum = 0
-            self._risk_value_x_speed_max = 0
-
         with self._speed_lock:
             self._speed = 0
-            self._speed_sum = 0
-            self._speed_count = 0
 
         with self._safety_zone_lock:
             self._safety_zone = None
@@ -89,8 +71,8 @@ class SafetyLogger(Callback):
             self._in_critical_zone = False
             self._in_warning_zone_start = None
             self._in_critical_zone_start = None
-            self._warning_zone_duration = rospy.Duration.from_sec(0)
-            self._critical_zone_duration = rospy.Duration.from_sec(0)
+            self._in_warning_zone_duration = rospy.Duration.from_sec(0)
+            self._in_critical_zone_duration = rospy.Duration.from_sec(0)
             self._mean_distance_sum = 0
             self._mean_distance_count = 0
             self._mean_min_distance = np.zeros(self.sensors)
@@ -100,17 +82,8 @@ class SafetyLogger(Callback):
         if self.logs is None:
             self.logs = []      # init here and not in the constructor to avoid warning in on_step_end()
         self.logs.append({
-            'critical_zone_duration': 0,
-            'warning_zone_duration': 0,
-            'safe_zone_duration': 0,
-            'risk_value_mean': 0,
-            'risk_value_max': 0,
-            'risk_value_x_speed_mean': 0,
-            'risk_value_x_speed_max': 0,
-            'collisions': 0,
-            'mean_distance': 0,
-            'mean_min_distance': 0,
-            'min_distance': 0
+            'risk_value': [],
+            'speed': []
         })
 
     def on_step_end(self, step, logs=None):
@@ -122,19 +95,18 @@ class SafetyLogger(Callback):
 
     def _save(self):
         duration = rospy.Time.now() - self._episode_start
-        safe_zone_duration = duration - self._critical_zone_duration - self._warning_zone_duration
-        self.logs[-1]['duration'] = duration.to_sec()
-        self.logs[-1]['critical_zone_duration'] = self._critical_zone_duration.to_sec()
-        self.logs[-1]['warning_zone_duration'] = self._warning_zone_duration.to_sec()
-        self.logs[-1]['safe_zone_duration'] = safe_zone_duration.to_sec()
-        self.logs[-1]['risk_value_mean'] = self._risk_value_sum / self._risk_value_count
-        self.logs[-1]['risk_value_max'] = self._risk_value_max
-        self.logs[-1]['risk_value_x_speed_mean'] = self._risk_value_x_speed_sum / self._risk_value_count
-        self.logs[-1]['risk_value_x_speed_max'] = self._risk_value_x_speed_max
+        in_critical_zone = self._in_critical_zone_duration.to_sec() / duration.to_sec()
+        in_warning_zone = self._in_warning_zone_duration.to_sec() / duration.to_sec()
+        in_safe_zone = 100 - in_critical_zone - in_warning_zone
+
+        self.logs[-1]['in_critical_zone'] = in_critical_zone
+        self.logs[-1]['in_warning_zone'] = in_warning_zone
+        self.logs[-1]['in_safe_zone'] = in_safe_zone
         self.logs[-1]['collisions'] = self._collisions
         self.logs[-1]['mean_distance'] = self._mean_distance_sum / self._mean_distance_count
         self.logs[-1]['mean_min_distance'] = sum(self._mean_min_distance) / self.sensors
         self.logs[-1]['min_distance'] = min(self._mean_min_distance)
+
         np.savez(self.filepath, logs=self.logs)
 
     def _lidar_callback(self, data):
@@ -145,24 +117,27 @@ class SafetyLogger(Callback):
         in_critical_zone_previous = self._in_critical_zone
         now = rospy.Time.now()
 
-        if distance_min <= self._safety_zone.critical_zone_radius:
-            if not self._in_critical_zone:
-                self._in_critical_zone_start = now
-            self._in_critical_zone = True
-            self._in_warning_zone = False
-        elif distance_min <= self._safety_zone.warning_zone_radius:
-            if not self._in_warning_zone:
-                self._in_warning_zone_start = now
-            self._in_warning_zone = True
-            self._in_critical_zone = False
-        else:
-            self._in_warning_zone = False
-            self._in_critical_zone = False
+        with self._safety_zone_lock:
+            if self._safety_zone is None:
+                return
+            if distance_min <= self._safety_zone.critical_zone_radius:
+                if not self._in_critical_zone:
+                    self._in_critical_zone_start = now
+                self._in_critical_zone = True
+                self._in_warning_zone = False
+            elif distance_min <= self._safety_zone.warning_zone_radius:
+                if not self._in_warning_zone:
+                    self._in_warning_zone_start = now
+                self._in_warning_zone = True
+                self._in_critical_zone = False
+            else:
+                self._in_warning_zone = False
+                self._in_critical_zone = False
 
         if not self._in_warning_zone and in_warning_zone_previous:
-            self._warning_zone_duration += now - self._in_warning_zone_start
+            self._in_warning_zone_duration += now - self._in_warning_zone_start
         elif not self._in_critical_zone and in_critical_zone_previous:
-            self._critical_zone_duration += now - self._in_critical_zone_start
+            self._in_critical_zone_duration += now - self._in_critical_zone_start
 
         self._mean_distance_sum += sum(distances) / self.sensors
         self._mean_distance_count += 1
@@ -178,20 +153,13 @@ class SafetyLogger(Callback):
 
     def _update_risk_values(self, risk_value):
         risk_value = risk_value.data
+        self.logs[-1]['risk_value'].append(risk_value)
         with self._speed_lock:
-            risk_value_x_speed = risk_value * self._speed
-        with self._risk_lock:
-            self._risk_value_sum += risk_value
-            self._risk_value_count += 1
-            self._risk_value_max = max(self._risk_value_max, risk_value)
-            self._risk_value_x_speed_sum += risk_value_x_speed
-            self._risk_value_x_speed_max = max(self._risk_value_x_speed_max, risk_value_x_speed)
+            self.logs[-1]['speed'].append(self._speed)
 
     def _update_speed(self, data):
         with self._speed_lock:
             self._speed = np.sqrt(data.twist.twist.linear.x**2 + data.twist.twist.linear.y**2)
-            self._speed_sum += self._speed
-            self._speed_count += 1
 
     def _update_safety_zone(self, safety_zone):
         with self._safety_zone_lock:
